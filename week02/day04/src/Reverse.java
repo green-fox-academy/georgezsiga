1361fb7aa32b71579329e3b719ba1dd57e6d4fc0
georgezsiga <georgezsiga@gmail.com>
2017-03-23 12:43:12 +0100

    Add initial upload of the Reverse excercise

---
+++ week02/day04/src/Reverse.java
@@ -0,0 +1,16 @@
+/**
+ * Created by georgezsiga on 3/23/17.
+ */
+public class Reverse {
+  public static void main(String... args) {
+    String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+    // Create a function that can reverse a String, which is passed as the parameter
+    // Use it on this reversed string to check it!
+    // Try to solve this using charAt() first, and optionally anything else after.
+
+    for (int i = reversed.length() - 1; i >= 0; i--) {
+      System.out.print(reversed.charAt(i));
+    }
+  }
+}
