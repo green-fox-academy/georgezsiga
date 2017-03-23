c04698ecd17b9916ba59953c255f564a4ddb2628
georgezsiga <georgezsiga@gmail.com>
2017-03-23 21:03:21 +0100

    Move the excercise description to the bottom for easier reading of the code

--- week02/day04/src/Calculator.java
+++ week02/day04/src/Calculator.java
@@ -3,21 +3,6 @@ import java.util.Scanner;
 /**
  * Created by georgezsiga on 3/23/17.
  */
-    // Create a simple calculator application which reads the parameters from the prompt
-    // and prints the result to the prompt.
-    // It should support the following operations:
-    // +, -, *, /, % and it should support two operands.
-    // The format of the expressions must be: {operation} {operand} {operand}.
-    // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
-
-    // You can use the Scanner class
-    // It should work like this:
-
-    // Start the program
-    // It prints: "Please type in the expression:"
-    // Waits for the user input
-    // Print the result to the prompt
-    // Exit
 
 public class Calculator {
   public static void main(String... args) {
@@ -57,3 +42,18 @@ public class Calculator {
     }
   }
 }
+    // Create a simple calculator application which reads the parameters from the prompt
+    // and prints the result to the prompt.
+    // It should support the following operations:
+    // +, -, *, /, % and it should support two operands.
+    // The format of the expressions must be: {operation} {operand} {operand}.
+    // Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+    // You can use the Scanner class
+    // It should work like this:
+
+    // Start the program
+    // It prints: "Please type in the expression:"
+    // Waits for the user input
+    // Print the result to the prompt
+    // Exit
