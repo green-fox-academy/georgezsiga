30b62e737e05028c246986d6a086a163ec27a44c
georgezsiga <georgezsiga@gmail.com>
2017-03-24 11:26:24 +0100

    Move the working algorythm to the Function in the PalindromeBuilder excercise

--- week02/day05/src/PalindromeBuilder.java
+++ week02/day05/src/PalindromeBuilder.java
@@ -8,10 +8,17 @@ public class PalindromeBuilder {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please give me a word and I make a Palindrome out of it");
     String userInput = scanner.nextLine();
-    String output = userInput;
-    for (int i = userInput.length()-1; i >= 0; i--) {
-      output = output + userInput.charAt(i);
+    createPalindrome(userInput);
+  }
+
+  private static void createPalindrome(String palindrome) {
+    String output = palindrome;
+    for (int i = palindrome.length() - 1; i >= 0; i--) {
+      output = output + palindrome.charAt(i);
     }
     System.out.println("Here is it: " + output + " ... pretty cool huh??");
   }
 }
+
+//  Create a function named create palindrome following your current language's style guide.
+//        It should take a string, create a palindrome from it and then return it.
