f40c013f54eff0a7bff8257c40538c1c8db88507
georgezsiga <georgezsiga@gmail.com>
2017-03-24 12:33:25 +0100

    Makes the GuessMyNumber fully interactive. The user can define any setting.

--- week02/day05/src/GuessMyNumber.java
+++ week02/day05/src/GuessMyNumber.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -6,13 +5,17 @@ import java.util.Scanner;
  */
 public class GuessMyNumber {
   public static void main(String[] arg) {
-    Random random = new Random();
-    int myNumber = random.nextInt(100) + 1;
 
-    System.out.println("Hi, lets play a guessing game.. how good are you? How many lives do you want to have?");
+    System.out.println("Hi, lets play a guessing game.. What should be the range I can think of? Maybe: 0  100?");
+    Scanner range = new Scanner(System.in);
+    int min = range.nextInt();
+    int max = range.nextInt();
+    int myNumber = (int) (Math.random() * max + min);
+
+    System.out.println("Nice! How many lives do you want to have?");
     Scanner scannerLife = new Scanner(System.in);
     int life = scannerLife.nextInt();
-    System.out.println("Good! I have a number in mind between 1 and 100. Can you guess what it is? You have " + life + " lives have to guess it.");
+    System.out.println("Good! I got a number between " + min + " and " + max + ". Can you guess what it is? You have " + life + " lives have to guess it.");
 
     while (life > 0) {
       Scanner scanner = new Scanner(System.in);
