070f61e6becceccd66c72d17a5bc777c8c4acf40
georgezsiga <georgezsiga@gmail.com>
2017-03-24 09:51:53 +0100

    Add the initial file of the Homework mateial review practice

---
+++ week02/day05/src/HomeworkExcercise.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+/**
+ * Created by georgezsiga on 3/24/17.
+ */
+public class HomeworkExcercise {
+  public static void(String[] args) {
+
+    ArrayList<Object> list = new ArrayList<>();
+    list.add(5);
+    (Integer)list.get(0);
+
+
+    Object myNumber = 5;
+    ((Integer)myNumber)  // it will behave like and integer if I know that it is a number inside
+  }
+}
