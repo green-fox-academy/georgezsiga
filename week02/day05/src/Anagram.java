81ebc76f4a9a3a845a18247e19c99d437165eab8
georgezsiga <georgezsiga@gmail.com>
2017-03-26 10:22:04 +0200

    Reverse the second word at Anagram exercise

--- week02/day05/src/Anagram.java
+++ week02/day05/src/Anagram.java
@@ -15,8 +15,10 @@ public class Anagram {
 
     String[] splitStr = userInput.split("\\s+");
     String firstWord = splitStr[0];
+    System.out.println(firstWord);
     String secondWord = splitStr[1];
-
+    for (int i = secondWord.length() - 1; i >= 0; i--) {
+      System.out.print(secondWord.charAt(i));
 //    System.out.println(firstWord);
 //    System.out.println(secondWord);
 
@@ -25,15 +27,27 @@ public class Anagram {
 //    int result = 0;
 //
 //
-    if (firstWord.length() != secondWord.length() ) {
-      System.out.println("false");
-      System.exit(0);
-    } /* else {
-      for (int i = 0; i < ; i++) {
-        */
+//    if (firstWord.length() != secondWord.length()) {
+//      System.out.println("false");
+//      System.exit(0);
+//    } else {
+//int i =0;
+//int j = firstWord.length();
+//      while (i == j)
+//      for (i = 0; i < secondWord.length(); i++) {
+//       for (j = firstWord.length() - 1; j >= 0; j--){
+//
+//          if (i == j) {
+//            System.out.println("true");
+//          } else {
+//            System.out.println("false");
+//          }
+//        }
       }
     }
-
+  }
+//}
+//  }
 
 //    String first = userInput.substring(0, userInput.indexOf(" "));
 //    String second = userInput.substring(userInput.indexOf(" ") + 1);
