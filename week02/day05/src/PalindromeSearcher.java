4942a1c4e8e635fc767c4affca4dfd0754c437c0
georgezsiga <georgezsiga@gmail.com>
2017-03-26 01:02:42 +0100

    Finds the palindromes in a single word at PalindromeSearch excercise

--- week02/day05/src/PalindromeSearcher.java
+++ week02/day05/src/PalindromeSearcher.java
@@ -8,16 +8,28 @@ public class PalindromeSearcher {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please give me some text and I wil find all the Palindromes in it");
     String userInput = scanner.nextLine();
-    createPalindrome(userInput);
-  }
+    userInput = userInput.toLowerCase();
+    int k = 1;
 
-  private static void createPalindrome(String palindrome) {
-    String output = palindrome;
-    while (palindrome.length() >= 3)
-    for (int i = palindrome.length() - 1; i >= 0; i--) {
-      output = output + palindrome.charAt(i);
+    if (userInput.length() == 3 && (userInput.charAt(0) == userInput.charAt(2))) {
+      System.out.println(userInput);
     }
-    System.out.println("Here is it: " + output + " ... pretty cool huh??");
+    while (k < userInput.length()-1) {
+      int i=1;
+      int j=1;
+      while (userInput.charAt(k - i) == userInput.charAt(k + 1)) {
+        for (i = k-i; i >0; i--) {
+          for (j = k+j; j < userInput.length(); j++) {
+            if (userInput.charAt(i) == userInput.charAt(j)) {
+//              String palindrome = userInput.substring(i, j+1);
+              System.out.println(userInput.substring(i, j+1));
+            }
+          }j=j+1;
+        }
+      }  k=k+1;
+    } /*if (userInput.charAt(userInput.length()-2) == userInput.charAt(userInput.length())) {
+      System.out.println(userInput.substring(userInput.length()-2, 3));
+    }*/
   }
 }
 //  Create a function named search palindrome following your current language's style guide.
