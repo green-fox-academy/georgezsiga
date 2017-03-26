300fbaf86c4c81921caa44f999230d111bb31f86
georgezsiga <georgezsiga@gmail.com>
2017-03-26 16:57:37 +0200

    Compare the two stings if they are equal at Anagram exercise

--- week02/day05/src/Anagram.java
+++ week02/day05/src/Anagram.java
@@ -12,33 +12,15 @@ public class Anagram {
     System.out.println("Please give me two words");
     String firstWord = scanner.next();
     String secondWord = scanner.next();
-    System.out.println(firstWord);
-    System.out.println(secondWord);
-
     String reverseSecondWord = "";
-    for (int i=secondWord.length()-1; i>=0; i--) {
+
+    for (int i = secondWord.length() - 1; i >= 0; i--) {
       reverseSecondWord = reverseSecondWord + secondWord.charAt(i);
-
     }
-    System.out.println(reverseSecondWord);
-
-    if (firstWord.length() != secondWord.length()) {
-      System.out.println("false");
-      System.exit(0);
+    if (firstWord.equalsIgnoreCase(reverseSecondWord)) {
+      System.out.println("true");
     } else {
-      int i = 0;
-      int j = firstWord.length();
-      while (i == j)
-        for (i = 0; i < secondWord.length(); i++) {
-          for (j = firstWord.length() - 1; j >= 0; j--) {
-
-            if (i == j) {
-              System.out.println("true");
-            } else {
-              System.out.println("false");
-            }
-          }
-        }
+      System.out.println("false");
     }
   }
 }
