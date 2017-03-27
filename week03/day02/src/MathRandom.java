1ecd43e1f017d007c52a626860c1997d6fb8e0c4
georgezsiga <georgezsiga@gmail.com>
2017-03-27 23:09:03 +0200

    Homework Excercise Practice

---
+++ week03/day02/src/MathRandom.java
@@ -0,0 +1,13 @@
+/**
+ * Created by georgezsiga on 3/27/17.
+ */
+public class MathRandom {
+  public static void main(String[] args) {
+//    (int)(Math.random() *10); // random between 0-9 0 included.. 10 not multiply by 10
+//    50 + (int)(Math.random() *50); // random between 50 -99
+//    int a =40;
+//    int b =55;
+//    a + Math.random() *b; // returns a random number between a and a+b excluding b
+
+  }
+}
