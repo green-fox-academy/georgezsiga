be9d93c8ddb4f87f150ff577158a879d4641d076
georgezsiga <georgezsiga@gmail.com>
2017-03-28 00:23:23 +0200

    Reverse the order of the lines ReversedLines

--- week03/day01/src/ReversedOrder.java
+++ week03/day01/src/ReversedOrder.java
@@ -7,17 +7,18 @@ import java.util.List;
  * Created by georgezsiga on 3/28/17.
  */
 public class ReversedOrder {
-  // Create a method that decrypts assets/reversed_zen_order.txt
-
-try {
-    // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
-    Path filePath = Paths.get("assets/reversed-order.txt");
-    List<String> lines = Files.readAllLines(filePath);
-
-
-
-  } catch (Exception e) {
-    System.out.println("Uh-oh, an error happend: " + e.getClass());
+  public static void main(String[] args) {
+    // Create a method that decrypts assets/reversed_zen_order.txt
+    try {
+      // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
+      Path filePath = Paths.get("assets/reversed-order.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      for (int j = lines.size(); j > 0; j--) {
+        System.out.println(lines.get(j));
+      }
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+    }
   }
 }
 
