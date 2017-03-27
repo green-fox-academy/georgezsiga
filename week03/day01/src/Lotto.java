6cf01944505f77251463621ae33b386e69ff802f
georgezsiga <georgezsiga@gmail.com>
2017-03-27 11:04:59 +0200

    Add initial file of Lotto.java

---
+++ week03/day01/src/Lotto.java
@@ -0,0 +1,11 @@
+/**
+ * Created by georgezsiga on 3/27/17.
+ */
+public class Lotto {
+  public static void main(String[] args) {
+    // Create a method that find the 5 most common lotto numbers assets/lotto.csv
+
+
+  }
+}
+
