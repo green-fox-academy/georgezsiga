472de5ddf091260aaf9ee83d0760399f81ecb7aa
georgezsiga <georgezsiga@gmail.com>
2017-03-28 00:54:40 +0200

    Prints the Encoded message at EncodedLines

--- week03/day01/src/EncodedLines.java
+++ week03/day01/src/EncodedLines.java
@@ -16,20 +16,19 @@ public class EncodedLines {
       Path filePath = Paths.get("assets/lorem_ipsum.txt");
       List<String> lines = Files.readAllLines(filePath);
       ArrayList aList = new ArrayList(Arrays.asList(lines.toString().split("(?!^)")));
-      ArrayList alphabet = new ArrayList(Arrays.toString["!","\"","\'","(",")","+",",","-",".","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+      ArrayList<String> ALPHABET = new ArrayList<>(Arrays.asList("!", "\"", "\'", "(", ")", "+", ",", "-", ".", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"));
       String decryptedLetter = new String();
       Object text = "";
       for (int i = 0; i < aList.size(); i++) {
         text = aList.get(i);
-        decryptedLetter = decryptedLetter + text;
+        int text2 = ALPHABET.indexOf(text) - 1;
+        Object text3 = ALPHABET.get(text2);
+        decryptedLetter = decryptedLetter + text3;
         aList.remove(text);
       }
       System.out.println(decryptedLetter);
-
-
     } catch (Exception e) {
       System.out.println("Uh-oh, an error happend: " + e.getClass());
     }
-
   }
 }
