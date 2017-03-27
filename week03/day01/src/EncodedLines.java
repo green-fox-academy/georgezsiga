416dbbd13593e3800928e8c6cf3eb431dc8ee0fc
georgezsiga <georgezsiga@gmail.com>
2017-03-28 00:47:11 +0200

    Ads the alphabet ReversedLines

---
+++ week03/day01/src/EncodedLines.java
@@ -0,0 +1,35 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by georgezsiga on 3/28/17.
+ */
+public class EncodedLines {
+  public static void main(String[] args) {
+    // Create a method that decrypts assets/encoded_zen_lines.txt
+    try {
+      // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
+      Path filePath = Paths.get("assets/lorem_ipsum.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      ArrayList aList = new ArrayList(Arrays.asList(lines.toString().split("(?!^)")));
+      ArrayList alphabet = new ArrayList(Arrays.toString["!","\"","\'","(",")","+",",","-",".","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+      String decryptedLetter = new String();
+      Object text = "";
+      for (int i = 0; i < aList.size(); i++) {
+        text = aList.get(i);
+        decryptedLetter = decryptedLetter + text;
+        aList.remove(text);
+      }
+      System.out.println(decryptedLetter);
+
+
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+    }
+
+  }
+}
