551da90ebd6ef05ba46e30025fd0b22be6d42486
georgezsiga <georgezsiga@gmail.com>
2017-03-28 00:00:54 +0200

    Initial file for ReversedLines

---
+++ week03/day01/src/ReversedLines.java
@@ -0,0 +1,26 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Created by georgezsiga on 3/27/17.
+ */
+public class ReversedLines {
+  public static void main(String[] args) {
+    // Create a method that decrypts assets/reversed_zen_lines.txt
+
+    try {
+      // Reads the content from `lorem_ipsum.txt` in the `assets` folder line by line to a String List
+      Path filePath = Paths.get("assets/reversed-lines.txt");
+      List<String> lines = Files.readAllLines(filePath);
+
+
+
+
+
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+    }
+  }
+}
