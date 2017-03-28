53d78e49fe7f0474eacfb026944c9e9392ceb919
georgezsiga <georgezsiga@gmail.com>
2017-03-28 13:57:37 +0200

    Add 3D effect to PurpleSteps3D

--- week03/day02/src/PurpleSteps3D.java
+++ week03/day02/src/PurpleSteps3D.java
@@ -14,15 +14,20 @@ public class PurpleSteps3D {
   }
 
   public static void drawline(Graphics graphics2) {
+//    int x = 0;
+//    while (x<=5) {
+//      x = x +0;
     int z = 10;
-    for (int i = 300; i >= 300-(19*z); ) {
-      i = i - z;
-      graphics2.setColor(Color.BLACK);
-      graphics2.fillRect(300 - i, 300 - i, z, z);
-      graphics2.setColor(new Color(153, 58, 164));
-      graphics2.fillRect(300 - i + 1, 300 - i + 1, z-2, z-2);
-      z =2*z;
-    }
+      for (int i = 300; i > 100; ) {
+        i = i - z;
+        graphics2.setColor(Color.BLACK);
+        graphics2.fillRect(300 - i, 300 - i, z, z);
+        graphics2.setColor(new Color(153, 58, 164));
+        graphics2.fillRect(300 - i + 1, 300 - i + 1, z - 2, z - 2);
+        z = 2 * z;
+      }
+//      x = x +1;
+//    }
 
   }
   //    Don't touch the code below
