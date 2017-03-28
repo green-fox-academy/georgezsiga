a4fc777f33e6e7e19dfb93a9b2552049ed943578
georgezsiga <georgezsiga@gmail.com>
2017-03-28 12:06:28 +0200

    Initial file of RainbowBox

---
+++ week03/day02/src/RainbowBox.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+/**
+ * Created by georgezsiga on 3/28/17.
+ */
+public class RainbowBox {
+  public static void mainDraw(Graphics graphics) {
+    // create a square drawing function that takes 2 parameters:
+    // the square size, and the fill color,
+    // and draws a square of that size and color to the center of the canvas.
+    // create a loop that fills the canvas with rainbow colored squares.
+
+    System.out.println("Give me a number between 0 and 300");
+  }
+
+  public static void drawline(Graphics graphics2) {
+    Scanner scanner = new Scanner(System.in);
+    int x = scanner.nextInt();
+    graphics2.fillRect((300-x)/2, (300-x)/2, x, x);
+  }
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 330));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
+}
