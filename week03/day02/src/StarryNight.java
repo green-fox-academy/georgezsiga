572b7bc61259e466e9ad265585ac13a3ccb2fb53
georgezsiga <georgezsiga@gmail.com>
2017-03-28 21:14:29 +0200

    Generate random numbers for the position of the stars

--- week03/day02/src/StarryNight.java
+++ week03/day02/src/StarryNight.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 /**
  * Created by georgezsiga on 3/28/17.
  */
 public class StarryNight {
-  public static void mainDraw(Graphics graphics){
+  final static int SCREENSIZE = 300;
+  final static int STARSIZE = 10;
+
+  public static void mainDraw(Graphics graphics) {
     // draw the night sky:
     // - The background should be black
     // - The stars can be small squares
@@ -13,7 +17,22 @@ public class StarryNight {
     // - The stars should have random color (some shade of grey)
 
 
+    int x = generateRandomNumber(graphics);
+    int y = generateRandomNumber(graphics);
+
   }
+
+  public static int generateRandomNumber(Graphics random){
+    Random rand = new Random();
+    int r = rand.nextInt(SCREENSIZE-STARSIZE) + 0;
+    return r;
+  }
+  public static void drawAstar(Graphics graphics, int x, int y){
+    graphics.setColor(Color.GREEN);
+    graphics.fillRect(x, y, STARSIZE, STARSIZE);
+  }
+
+
 
   //    Don't touch the code below
   public static void main(String[] args) {
