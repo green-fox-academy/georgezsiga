5a1fea3665c446abc4dfd8c659d37e7991250d5c
georgezsiga <georgezsiga@gmail.com>
2017-03-28 16:29:11 +0200

    Add the polygon function to ConnectTheDots

--- week03/day02/src/ConnectTheDots.java
+++ week03/day02/src/ConnectTheDots.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.List;
+
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 /**
@@ -15,8 +19,28 @@ public class ConnectTheDots {
     // {120, 100}, {85, 130}, {50, 100}}
 
 
-
+    polygon(graphics);
   }
+
+  public static void polygon(Graphics graphics3){
+    int xpoints[] = {25, 145, 25, 145, 25};
+    int ypoints[] = {25, 25, 145, 145, 25};
+    int npoints = 5;
+
+    graphics3.drawPolygon(xpoints, ypoints, npoints);
+  }
+
+//  public static void drawline(Graphics graphics2) {
+//    List<Integer> coordinates = new ArrayList<>();
+//    List<Integer> temp = Arrays.asList(10, 10, 20, 20);
+//
+//    coordinates.addAll(temp);
+//    graphics2.setColor(Color.BLACK);
+//    graphics2.drawLine(coordinates.indexOf(0), coordinates.indexOf(1), coordinates.indexOf(2), coordinates.indexOf(3));
+//  }
+
+
+
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
