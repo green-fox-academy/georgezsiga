d79845846d62ede3d8935e2fe89de33221bb028a
georgezsiga <georgezsiga@gmail.com>
2017-03-28 23:44:48 +0200

    Add the base big triangles

--- week03/day02/src/Triangles.java
+++ week03/day02/src/Triangles.java
@@ -6,13 +6,43 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  * Created by georgezsiga on 3/28/17.
  */
 public class Triangles {
-  public static void mainDraw(Graphics graphics){
+  final public static int TIPOFTRIANGLEX = 150;
+  final public static int TIPOFTRIANGLEY = 0;
+  public static void mainDraw(Graphics graphics) {
     // reproduce this:
     // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/triangles/r5.png]
 
+//for (int slide = TIPOFTRIANGLEX-5; slide > 0;) {
+//  slide-=5;
+//  for (int down = TIPOFTRIANGLEY+10; down < 300;) {
+//    down+=10;
+    for (int i = TIPOFTRIANGLEX; i < 290; ) {
+      for (int j = TIPOFTRIANGLEY; j < 290; ) {
+        drawAtriangle(graphics, i, j);
+        i += 5;
+        j += 10;
+      }
+    }
+        for (int i = TIPOFTRIANGLEX-5; i > 10; ) {
+          for (int j = TIPOFTRIANGLEY+10; j < 290; ) {
+            drawAtriangle(graphics, i, j);
+            i -= 5;
+            j += 10;
+          }
+        }
+      }
 
 
+//}
+//  }
+
+  public static void drawAtriangle(Graphics graphics, int i, int j){
+    int xpoints[] = {i, i-5, i+5};
+    int ypoints[] = {j, j+10, j+10};
+    int npoints = 3;
+    graphics.drawPolygon(xpoints, ypoints, npoints);
   }
+
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
