6d22cb89cca7c67c208ea0d89828094915a637fb
georgezsiga <georgezsiga@gmail.com>
2017-03-28 11:30:33 +0200

    Draw a green box to the center of SquareInTheCenter

--- week03/day02/src/SquareInTheCenter.java
+++ week03/day02/src/SquareInTheCenter.java
@@ -8,15 +8,14 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class SquareInTheCenter {
   public static void mainDraw(Graphics graphics){
     // draw a green 10x10 square to the canvas' center.
-
-
-
+    graphics.setColor(Color.GREEN);
+    graphics.drawRect(145, 145, 10, 10);
   }
 
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setSize(new Dimension(320, 343));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
