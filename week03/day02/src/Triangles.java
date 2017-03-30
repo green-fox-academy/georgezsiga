54265d1568972c0487b2991a40fd4f92729451e7
georgezsiga <georgezsiga@gmail.com>
2017-03-30 23:22:52 +0200

    Replaces the equations with a recursive function..

--- week03/day02/src/Triangles.java
+++ week03/day02/src/Triangles.java
@@ -6,47 +6,31 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  * Created by georgezsiga on 3/28/17.
  */
 public class Triangles {
-  final public static int TIPOFTRIANGLEX = 150;
-  final public static int TIPOFTRIANGLEY = 0;
   public static void mainDraw(Graphics graphics) {
-    // reproduce this:
-    // [https://github.com/greenfox-academy/teaching-materials/blob/master/exercises/drawing/triangles/r5.png]
-    for (int inside = 0; inside < 145;) {
-      for (int insideOther = 0; insideOther < 290;) {
-        for (int i = TIPOFTRIANGLEX; i > 0;) {
-          for (int j = TIPOFTRIANGLEY; j < 300;) {
-            if (j+insideOther < 300 || i+inside < 0) {
-            drawAtriangle(graphics, i, j, insideOther, inside);
-            i -= 5;
-            j += 10;
-            } else {
-              i -= 5;
-              j += 10;
-            }
-          }
-        }
-        inside += 5;
-        insideOther += 10;
-      }
-    }
-    graphics.setColor(Color.BLACK);
-    int xpoints[] = {295, 290, 300};
-    int ypoints[] = {290, 300, 300};
+    fractals(graphics, 150, 0, 10);
+  }
+
+  public static void drawAtriangle(Graphics graphics, int i, int j, int l){
+    int xpoints[] = {i+l, i, i+(2*l)};
+    int ypoints[] = {j, j+(2*l), j+(2*l)};
     int npoints = 3;
     graphics.drawPolygon(xpoints, ypoints, npoints);
   }
 
-  public static void drawAtriangle(Graphics graphics, int i, int j, int insideOther, int inside){
-    int xpoints[] = {i+inside, i-5+inside, i+5+inside};
-    int ypoints[] = {j+insideOther, j+10+insideOther, j+10+insideOther};
-    int npoints = 3;
-    graphics.drawPolygon(xpoints, ypoints, npoints);
+  public static void fractals(Graphics graphics, int i, int j, int l) {
+    if (i <= 0 ||  j > 280) {
+      return;
+    } else {
+      drawAtriangle(graphics, i, j, l);
+      fractals(graphics, i - 10, j + 20, l);
+      fractals(graphics, i + 10, j + 20, l);
+    }
   }
 
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
-    jFrame.setSize(new Dimension(300, 330));
+    jFrame.setSize(new Dimension(400, 400));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
