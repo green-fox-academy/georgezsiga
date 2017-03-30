ac727fe50762b3147abb4423debf06d4be05293a
georgezsiga <georgezsiga@gmail.com>
2017-03-30 12:28:54 +0200

    Changes the message for the user

--- week03/day04/src/String2.java
+++ week03/day04/src/String2.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class String2 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Give me the number of bunnies");
+    System.out.println("Give me some text");
     String text = scanner.nextLine();
     System.out.println(modifiedText(text));
   }
