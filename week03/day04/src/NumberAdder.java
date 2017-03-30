636a86bf015d48b752d449080cb3d651301a99a2
georgezsiga <georgezsiga@gmail.com>
2017-03-30 10:13:27 +0200

    Initial file and solution for adding numbers with recursion

---
+++ week03/day04/src/NumberAdder.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by georgezsiga on 3/30/17.
+ */
+// Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+public class NumberAdder {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me a number");
+    int number = scanner.nextInt();
+    System.out.println(count(number));
+  }
+
+  public static int count(int number) {
+    int count = 0;
+    if (number == 0) {
+      return 0;
+    } else {
+      count = count + number;
+      return number + count(--number);
+    }
+  }
+}
