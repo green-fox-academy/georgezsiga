7ba59b330c5a0447d9a4d0137119c3d4fa4cfe4d
georgezsiga <georgezsiga@gmail.com>
2017-03-30 11:03:36 +0200

    Initial file

---
+++ week03/day04/src/BunniesAgain.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by georgezsiga on 3/30/17.
+ */
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+public class BunniesAgain {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me the number of bunnies");
+    int bunnies = scanner.nextInt();
+    System.out.println(bunniesEars(bunnies));
+  }
+
+  public static int bunniesEars(int bunnies) {
+    if (bunnies == 0) {
+      return 0;
+    } else {
+      return 2 + bunniesEars(--bunnies);
+    }
+  }
+}
