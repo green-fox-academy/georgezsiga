c5904212ef28b95852a994a8d51edb1a506b3f52
georgezsiga <georgezsiga@gmail.com>
2017-03-30 13:05:59 +0200

    Add initial file

---
+++ week03/day04/src/YellowBoxes.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+/**
+ * Created by georgezsiga on 3/30/17.
+ */
+public class YellowBoxes {
+    public static void mainDraw(Graphics graphics) {
+      // fill the canvas with a checkerboard pattern.
+      drawline(graphics);
+    }
+
+    public static void drawline(Graphics graphics2) {
+      int z = 20;
+      for (int k = 300; k >= 0 ;) {
+        for (int i = 310; i >= 0; ) {
+          graphics2.setColor(Color.BLACK);
+          graphics2.fillRect(300 - i, 300-k, 10, 10);
+          i = i - z;
+          for (int j = 310; j >= 0; ) {
+            graphics2.setColor(Color.BLACK);
+            graphics2.fillRect(300-k, 300 - j, 10, 10);
+            j = j - z;
+          }
+        }
+        k = k - z;
+      }
+    }
+
+    //    Don't touch the code below
+    public static void main(String[] args) {
+      JFrame jFrame = new JFrame("Drawing");
+      jFrame.setSize(new Dimension(300, 330));
+      jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+      jFrame.add(new ImagePanel());
+      jFrame.setLocationRelativeTo(null);
+      jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+      @Override
+      protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        mainDraw(graphics);
+
+      }
+    }
+
+  }
