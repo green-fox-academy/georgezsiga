14306dcaf43a9148a4d559d24c11f9dc2add214b
georgezsiga <georgezsiga@gmail.com>
2017-03-30 13:18:04 +0200

    Add lines into the box

--- week03/day04/src/YellowBoxes.java
+++ week03/day04/src/YellowBoxes.java
@@ -8,18 +8,24 @@ public class YellowBoxes {
     public static void mainDraw(Graphics graphics) {
       // fill the canvas with a checkerboard pattern.
       drawBox(graphics);
+      drawLine(graphics);
     }
 
     public static void drawBox(Graphics graphics) {
       graphics.setColor(Color.YELLOW);
       graphics.fillRect(0, 0, 600, 600);
+      graphics.setColor(Color.BLACK);
+      graphics.drawRect(0, 0, 600, 600);
+
     }
 
-//    public static void drawLine(Graphics graphics) {
-//      graphics.drawLine();
-//
-//
-//    }
+    public static void drawLine(Graphics graphics) {
+      graphics.setColor(Color.BLACK);
+      graphics.drawLine(0, 200, 600, 200);
+      graphics.drawLine(0, 400, 600, 400);
+      graphics.drawLine(200, 0, 200, 600);
+      graphics.drawLine(400, 0, 400, 600);
+    }
 
     //    Don't touch the code below
     public static void main(String[] args) {
