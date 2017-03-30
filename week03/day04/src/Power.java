e7db0d31fc34f9e32fa2c43a524820da27f8e7bc
georgezsiga <georgezsiga@gmail.com>
2017-03-30 10:40:13 +0200

    Initial file and user input

---
+++ week03/day04/src/Power.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by georgezsiga on 3/30/17.
+ */
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+public class Power {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me two numbers - base and n-th power");
+    int base = scanner.nextInt();
+    int nPower = scanner.nextInt();
+
+    System.out.println(power(base, nPower));
+  }
+
+  public static int power(int base, int nPower) {
+
+    if (nPower == 0) {
+      return base;
+    } else {
+      return base * base;
+    }
+  }
+}
