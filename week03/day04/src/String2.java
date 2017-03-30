974aeb31eb654c8f17fccabb86c694388eacc958
georgezsiga <georgezsiga@gmail.com>
2017-03-30 12:07:35 +0200

    Initial file and recursive solution

---
+++ week03/day04/src/String2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by georgezsiga on 3/30/17.
+ */
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+public class String2 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Give me the number of bunnies");
+    String text = scanner.nextLine();
+    System.out.println(modifiedText(text));
+  }
+
+  public static String modifiedText(String text) {
+    if (text.contains("x")) {
+      String new1 = text.substring(0, text.indexOf("x"));
+      String new2 = text.substring(text.indexOf("x")+1);
+      text = new1 + new2;
+      return modifiedText(text);
+    } else {
+      return text;
+    }
+  }
+}
