d9b17471598717f71de5af00682e822081bf65ca
georgezsiga <georgezsiga@gmail.com>
2017-03-31 09:44:34 +0200

    Add the draw function for the equilateral triangle

--- week03/day05/src/Triangles.java
+++ week03/day05/src/Triangles.java
@@ -1,36 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static java.lang.Math.sqrt;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 /**
  * Created by georgezsiga on 3/31/17.
  */
 public class Triangles {
-
   public static void mainDraw(Graphics graphics) {
-    fractals(graphics, 300, 0, 100);
+    drawAtriangle(graphics, 450, 450, 150, 130 );
+//    fractals(graphics, 300, 0, 100);
   }
 
-  public static void drawAtriangle(Graphics graphics, int i, int j, int l) {
-    int xpoints[] = {i + l, i, i + (2 * l)};
-    int ypoints[] = {j, j + (2 * l), j + (2 * l)};
+//  public static void drawAtriangle(Graphics graphics, int i, int j, int l) {
+//    int xpoints[] = {i + l, i, i + (2 * l)};
+//    int ypoints[] = {j, j + (2 * l), j + (2 * l)};
+//    int npoints = 3;
+//    graphics.drawPolygon(xpoints, ypoints, npoints);
+//  }
+
+  public static void drawAtriangle(Graphics graphics, int x, int y, int l, int h) {
+    int xpoints[] = {x, x - l, x + l};
+    int ypoints[] = {y - h, y + h, y + h};
     int npoints = 3;
     graphics.drawPolygon(xpoints, ypoints, npoints);
   }
 
-  public static void fractals(Graphics graphics, int i, int j, int l) {
+
+  public static void fractals(Graphics graphics, int x, int y, int l, int h) {
     if (l < 3) {
       return;
     } else {
-      drawAtriangle(graphics, i, j, l);
-      fractals(graphics, i/3, j, l/2);
+      drawAtriangle(graphics, x, y, l, h);
+      fractals(graphics, x / 3, y, l / 2, h);
     }
   }
 
   //    Don't touch the code below
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Triangles");
-    jFrame.setSize(new Dimension(600, 630));
+    jFrame.setSize(new Dimension(900, 930));
     jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     jFrame.add(new Triangles.ImagePanel());
     jFrame.setLocationRelativeTo(null);
