d395d2c481f69618f79014373a13a20ca1416df7
georgezsiga <georgezsiga@gmail.com>
2017-03-31 11:01:44 +0200

    Reformat code to Google standards

--- week03/day05/src/SquareGrid.java
+++ week03/day05/src/SquareGrid.java
@@ -12,13 +12,14 @@ import javax.swing.JPanel;
  * Created by georgezsiga on 3/31/17.
  */
 public class SquareGrid {
+
   public static void mainDraw(Graphics graphics) {
     drawBox(graphics, 400, 400, 100, 20);
     fractals(graphics, 400, 400, 100, 20);
   }
 
   public static void drawBox(Graphics graphics, int x, int y, int l, float w) {
-    Graphics2D g2 = (Graphics2D)graphics;
+    Graphics2D g2 = (Graphics2D) graphics;
     g2.setStroke(new BasicStroke(w));
     graphics.drawRect(x - l, y - l, 2 * l, 2 * l);
   }
@@ -29,13 +30,13 @@ public class SquareGrid {
     } else {
       drawBox(graphics, x, y, l, w);
       graphics.setColor(Color.GREEN);
-      fractals(graphics, x-l, y-l, l /2, w/2);
+      fractals(graphics, x - l, y - l, l / 2, w / 2);
       graphics.setColor(Color.BLUE);
-      fractals(graphics, x+l, y+l, l /2, w/2);
+      fractals(graphics, x + l, y + l, l / 2, w / 2);
       graphics.setColor(Color.YELLOW);
-      fractals(graphics, x+l, y-l, l /2, w/2);
+      fractals(graphics, x + l, y - l, l / 2, w / 2);
       graphics.setColor(Color.RED);
-      fractals(graphics, x-l, y+l, l /2, w/2);
+      fractals(graphics, x - l, y + l, l / 2, w / 2);
     }
   }
 
