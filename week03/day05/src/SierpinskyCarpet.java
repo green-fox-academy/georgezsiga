6e9da6c90a4dae7749fe94714a6c382d1fecd251
georgezsiga <georgezsiga@gmail.com>
2017-03-31 10:38:17 +0200

    Add colors and cleans up the code

--- week03/day05/src/SierpinskyCarpet.java
+++ week03/day05/src/SierpinskyCarpet.java
@@ -10,14 +10,13 @@ import javax.swing.JPanel;
  * Created by georgezsiga on 3/31/17.
  */
 public class SierpinskyCarpet {
+
   public static void mainDraw(Graphics graphics) {
-    drawBox(graphics, 400, 400, 100);
     fractals(graphics, 400, 400, 100);
   }
 
   public static void drawBox(Graphics graphics, int x, int y, int l) {
-    graphics.setColor(Color.RED);
-    graphics.fillRect(x-l, y-l, 2*l, 2*l);
+    graphics.fillRect(x - l, y - l, 2 * l, 2 * l);
   }
 
   public static void fractals(Graphics graphics, int x, int y, int l) {
@@ -26,14 +25,21 @@ public class SierpinskyCarpet {
     } else {
       drawBox(graphics, x, y, l);
       graphics.setColor(Color.GREEN);
-      fractals(graphics, x-(2*l), y, l/3);
-      fractals(graphics, x-(2*l), y-(2*l), l/3);
-      fractals(graphics, x+(2*l), y, l/3);
-      fractals(graphics, x+(2*l), y+(2*l), l/3);
-      fractals(graphics, x, y+(2*l), l/3);
-      fractals(graphics, x, y-(2*l), l/3);
-      fractals(graphics, x+(2*l), y-(2*l), l/3);
-      fractals(graphics, x-(2*l), y+(2*l), l/3);
+      fractals(graphics, x - (2 * l), y, l / 3);
+      graphics.setColor(Color.BLUE);
+      fractals(graphics, x - (2 * l), y - (2 * l), l / 3);
+      graphics.setColor(Color.LIGHT_GRAY);
+      fractals(graphics, x + (2 * l), y, l / 3);
+      graphics.setColor(Color.YELLOW);
+      fractals(graphics, x + (2 * l), y + (2 * l), l / 3);
+      graphics.setColor(Color.ORANGE);
+      fractals(graphics, x, y + (2 * l), l / 3);
+      graphics.setColor(Color.PINK);
+      fractals(graphics, x, y - (2 * l), l / 3);
+      graphics.setColor(Color.RED);
+      fractals(graphics, x + (2 * l), y - (2 * l), l / 3);
+      graphics.setColor(Color.MAGENTA);
+      fractals(graphics, x - (2 * l), y + (2 * l), l / 3);
     }
   }
 
@@ -48,6 +54,7 @@ public class SierpinskyCarpet {
   }
 
   static class ImagePanel extends JPanel {
+
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
