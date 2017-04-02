df760dc4be5f5a85fc56c5f2b66f3eeea2518d6f
georgezsiga <georgezsiga@gmail.com>
2017-04-02 12:47:45 +0200

    Add initial file and the data for the five trees

---
+++ week04/day01/src/FiveTrees.java
@@ -0,0 +1,36 @@
+/**
+ * Created by georgezsiga on 4/2/17.
+ */
+public class FiveTrees {
+  public static void main(String args[]) {
+    String nameOfTree1 = new String("big tree");
+    String nameOfTree2 = new String("small tree");
+    String nameOfTree3 = new String("beautiful tree");
+    String nameOfTree4 = new String("my favourite tree");
+    String nameOfTree5 = new String("just a tree");
+
+    String leafColorOfTree1 = new String("blue");
+    String leafColorOfTree2 = new String("red");
+    String leafColorOfTree3 = new String("green");
+    String leafColorOfTree4 = new String("purple");
+    String leafColorOfTree5 = new String("black");
+
+    int ageOfTree1 = 40;
+    int ageOfTree2 = 4;
+    int ageOfTree3 = 25;
+    int ageOfTree4 = 32;
+    int ageOfTree5 = 10;
+
+    String sexOfTree1 = new String("male");
+    String sexOfTree2 = new String("female");
+    String sexOfTree3 = new String("female");
+    String sexOfTree4 = new String("female");
+    String sexOfTree5 = new String("male");
+
+    System.out.println("The name of my first tree is: " + nameOfTree1 + " It is " + ageOfTree1 + " years old, it has " + leafColorOfTree1 + " leaves, and it is a " + sexOfTree1 + " tree.");
+    System.out.println("The name of my first tree is: " + nameOfTree2 + " It is " + ageOfTree2 + " years old, it has " + leafColorOfTree2 + " leaves, and it is a " + sexOfTree2 + " tree.");
+    System.out.println("The name of my first tree is: " + nameOfTree3 + " It is " + ageOfTree3 + " years old, it has " + leafColorOfTree3 + " leaves, and it is a " + sexOfTree3 + " tree.");
+    System.out.println("The name of my first tree is: " + nameOfTree4 + " It is " + ageOfTree4 + " years old, it has " + leafColorOfTree4 + " leaves, and it is a " + sexOfTree4 + " tree.");
+    System.out.println("The name of my first tree is: " + nameOfTree5 + " It is " + ageOfTree5 + " years old, it has " + leafColorOfTree5 + " leaves, and it is a " + sexOfTree5 + " tree.");
+  }
+}
