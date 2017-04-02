7df04525727c280f1085718cdaf004a50f45a18b
georgezsiga <georgezsiga@gmail.com>
2017-04-03 00:14:50 +0200

    Set random colors and cleans up the code

--- week03/day05/src/KochLine.java
+++ week03/day05/src/KochLine.java
@@ -10,6 +10,7 @@ import javax.swing.JPanel;
  * Created by georgezsiga on 4/3/17.
  */
 public class KochLine {
+
   public static void mainDraw(Graphics graphics) {
     fractals(graphics, 30, 300, 0, 180, 3);
   }
@@ -24,32 +25,28 @@ public class KochLine {
     if (loop == 0) {
       return;
     } else {
-      graphics.setColor(Color.BLACK);
-//      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
+      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
       double x = startX + length * Math.cos(Math.toRadians(angle));
       double y = startY + length * Math.sin(Math.toRadians(angle));
       graphics.drawLine((int) startX, (int) startY, (int) x, (int) y);
-//      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
       graphics.setColor(Color.WHITE);
       double xw = x + length * Math.cos(Math.toRadians(angle));
       double yw = y + length * Math.sin(Math.toRadians(angle));
       graphics.drawLine((int) x, (int) y, (int) xw, (int) yw);
-      graphics.setColor(Color.BLACK);
-//      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
-      double x1 = x + length * Math.cos(Math.toRadians(angle-60));
-      double y1 = y + length * Math.sin(Math.toRadians(angle-60));
+      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
+      double x1 = x + length * Math.cos(Math.toRadians(angle - 60));
+      double y1 = y + length * Math.sin(Math.toRadians(angle - 60));
       graphics.drawLine((int) x, (int) y, (int) x1, (int) y1);
-//      graphics.setColor(new Color(randomColor(), randomColor(), randomColor()));
-      double x2 = x1 + length * Math.cos(Math.toRadians(angle+60));
-      double y2 = y1 + length * Math.sin(Math.toRadians(angle+60));
+      double x2 = x1 + length * Math.cos(Math.toRadians(angle + 60));
+      double y2 = y1 + length * Math.sin(Math.toRadians(angle + 60));
       graphics.drawLine((int) x1, (int) y1, (int) x2, (int) y2);
       double x3 = x2 + length * Math.cos(Math.toRadians(angle));
       double y3 = y2 + length * Math.sin(Math.toRadians(angle));
       graphics.drawLine((int) x2, (int) y2, (int) x3, (int) y3);
-      fractals(graphics, startX, startY, angle, length/3, loop - 1);
-      fractals(graphics, x2, y2, angle, length/3, loop - 1);
-      fractals(graphics, x, y, angle - 60, length/3, loop - 1);
-      fractals(graphics, x1, y1, angle + 60, length/3, loop - 1);
+      fractals(graphics, startX, startY, angle, length / 3, loop - 1);
+      fractals(graphics, x2, y2, angle, length / 3, loop - 1);
+      fractals(graphics, x, y, angle - 60, length / 3, loop - 1);
+      fractals(graphics, x1, y1, angle + 60, length / 3, loop - 1);
     }
   }
 
