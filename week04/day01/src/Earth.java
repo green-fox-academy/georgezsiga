b1ff5a5cc2c031c5718a99da24afb8fadfc0193b
georgezsiga <georgezsiga@gmail.com>
2017-04-02 11:05:21 +0200

    Creates Human objects Tom and  Joe

---
+++ week04/day01/src/Earth.java
@@ -0,0 +1,22 @@
+/**
+ * Created by georgezsiga on 4/2/17.
+ */
+public class Earth {
+  public static void main(String args[]) {
+    Human tom;
+    tom = new Human();
+    tom.age = 5;
+    tom.eyeColor = "blue";
+    tom.heightInCms = 122;
+    tom.name = "Tom Tobias";
+    tom.speak();
+
+    Human joe;
+    tom = new Human();
+    tom.age = 10;
+    tom.eyeColor = "green";
+    tom.heightInCms = 155;
+    tom.name = "Joe Joseph";
+    tom.speak();
+  }
+}
