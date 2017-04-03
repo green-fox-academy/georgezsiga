cea54f74cc9537d9207f611d1e9e547fbd39da05
georgezsiga <georgezsiga@gmail.com>
2017-04-03 16:29:40 +0200

    Add remove function

--- week04/day01/src/SharpieSet.java
+++ week04/day01/src/SharpieSet.java
@@ -5,29 +5,47 @@ import java.util.List;
  * Created by georgezsiga on 4/3/17.
  */
 public class SharpieSet {
-  List<String> sharpies;
-  public boolean countUsable;
 
-  public void countUsable() {
-      this.countUsable = true;
-    }
+  public List<Sharpie> sharpies;
 
-  public void removeTrash() {
-
+  public SharpieSet() {
+    this.sharpies = new ArrayList<>();
   }
 
-  public static void main(String[] args) {
-    List<Sharpie> sharpies = initializeSharpies();
-
-    System.out.println(sharpies);
-  }
-
-  static List<Sharpie> initializeSharpies() {
-    List<Sharpie> sharpies = new ArrayList<>();
-    sharpies.add(new Sharpie("blue", 2));
+  public void add() {
+    sharpies.add(new Sharpie("blue", 4));
     sharpies.add(new Sharpie("red", 6));
     sharpies.add(new Sharpie("green", 5));
     sharpies.add(new Sharpie("black", 7));
-    return sharpies;
   }
+
+  public int countUsable() {
+    int count = 0;
+    for (int i = 0; i < sharpies.size(); i++) {
+      if (sharpies.get(i).inkAmount > 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public void removeTrash() {
+    for (int i = 0; i < sharpies.size();) {
+      if (sharpies.get(i).inkAmount != 0) {
+        i++;
+      }
+      sharpies.remove(i);
+    }
+  }
+
+
+  public static void main(String[] args) {
+    SharpieSet sharpieSet = new SharpieSet();
+    sharpieSet.add();
+    sharpieSet.countUsable();
+    sharpieSet.removeTrash();
+    System.out.println(sharpieSet.sharpies);
+  }
+
+
 }
