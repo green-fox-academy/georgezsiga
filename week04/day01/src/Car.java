ffb5f9e8753e1cdd48ca7182db9e743162d847b6
georgezsiga <georgezsiga@gmail.com>
2017-04-03 14:04:56 +0200

    Reformat code

---
+++ week04/day01/src/Car.java
@@ -0,0 +1,14 @@
+/**
+ * Created by georgezsiga on 4/3/17.
+ */
+public class Car {
+
+  int gasAmount;
+  int capacity;
+  String name;
+
+  public Car(String name) {
+    this.gasAmount = 0;
+    this.capacity = 100;
+  }
+}
