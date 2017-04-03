450546c1ab9e84fb3be7a03a4ecaf7ad58fb06f6
georgezsiga <georgezsiga@gmail.com>
2017-04-03 11:57:09 +0200

    Add initial file

---
+++ week04/day01/src/FleetOfThings.java
@@ -0,0 +1,16 @@
+/**
+ * Created by georgezsiga on 4/3/17.
+ */
+public class FleetOfThings {
+
+  public static void main(String[] args) {
+    Fleet fleet = new Fleet();
+    // Create a fleet of things to have this output:
+    // 1. [ ] Get milk
+    // 2. [ ] Remove the obstacles
+    // 3. [x] Stand up
+    // 4. [x] Eat lunch
+
+    System.out.println(fleet);
+  }
+}
