b51baffb8ecdb9bcdce96951e18a1e44960abde0
georgezsiga <georgezsiga@gmail.com>
2017-04-03 12:23:54 +0200

    Add values and object and print it out

---
+++ week04/day01/src/Animal.java
@@ -0,0 +1,41 @@
+/**
+ * Created by georgezsiga on 4/3/17.
+ */
+public class Animal {
+
+  int hunger;
+  int thirst;
+  String name;
+
+  @Override
+  public String toString() {
+    return name +
+        ": hunger=" + hunger +
+        ", thirst=" + thirst;
+  }
+
+  public Animal(String name) {
+    hunger = 50;
+    thirst = 50;
+    this.name = name;
+  }
+
+  public void eat() {
+    this.hunger -= 1;
+  }
+
+  public void drink() {
+    this.thirst -= 1;
+  }
+
+  public void play() {
+    this.hunger += 1;
+    this.thirst -= 1;
+  }
+
+  public static void main(String[] args) {
+    Animal lion = new Animal("Lion");
+    System.out.println(lion.toString());
+
+  }
+}
