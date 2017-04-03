cff8f260bc9232347afe134f8e744ca7cb9fe0b8
georgezsiga <georgezsiga@gmail.com>
2017-04-03 18:25:50 +0200

    Add slaughter method

--- week04/day01/src/Farm.java
+++ week04/day01/src/Farm.java
@@ -1,14 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by georgezsiga on 4/3/17.
  */
 public class Farm {
-  int slots;
+  public List<Animal> animals;
+  int slots = 10;
+
+  public Farm() {
+    this.animals = new ArrayList<>();
+  }
+
+  public void add() {
+    animals.add(new Animal("lion"));
+    animals.add(new Animal("goat"));
+    animals.add(new Animal("chicken"));
+  }
 
   public void breed() {
+    if (slots > animals.size()) {
+      animals.add(new Animal("new animal"));
+    }
     slots -=1;
   }
 
   public void slaughter() {
-    slots +=1;
+    Animal tempAnimal = animals.get(0);
+    for (Animal animal : animals) {
+        if (animal.hunger < tempAnimal.hunger) {
+          tempAnimal = animal;
+        }
+      }
+      animals.remove(tempAnimal);
+    }
+
+  public static void main(String[] args) {
+    Farm farmAnimals = new Farm();
+    farmAnimals.add();
+    farmAnimals.breed();
+    farmAnimals.animals.get(2).hunger = 30;
+    farmAnimals.slaughter();
+    System.out.println(farmAnimals.animals);
   }
 }
