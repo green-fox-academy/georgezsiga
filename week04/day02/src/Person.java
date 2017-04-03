de8c36d962e96febe52e13af88fabf6ddeed3bdf
georgezsiga <georgezsiga@gmail.com>
2017-04-03 22:10:29 +0200

    Add initial file

---
+++ week04/day02/src/Person.java
@@ -0,0 +1,28 @@
+/**
+ * Created by georgezsiga on 4/3/17.
+ */
+public class Person {
+  String name;
+  int age;
+  String gender;
+
+  public void introduce() {
+
+  }
+
+  public void getGoal() {
+
+  }
+
+  public Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  public Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+}
