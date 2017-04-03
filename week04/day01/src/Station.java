5f46dd7a06c00571274cb7453be03d659a97c071
georgezsiga <georgezsiga@gmail.com>
2017-04-03 15:36:16 +0200

    Add station gas Amount status

--- week04/day01/src/Station.java
+++ week04/day01/src/Station.java
@@ -6,12 +6,20 @@ public class Station {
   int gasAmount;
 
   public void refill(Car name) {
+    if (gasAmount > (name.capacity - name.gasAmount)) {
     this.gasAmount = gasAmount - (name.capacity - name.gasAmount);
     name.gasAmount = name.gasAmount + (name.capacity - name.gasAmount);
+    }
+    System.out.println("Sorry, out of gas");
   }
 
   public static void main(String[] args) {
     Car cadillac = new Car("Cadillac");
+
+    Station station = new Station();
+    station.gasAmount = 40;
+    station.refill(cadillac);
     System.out.println(cadillac.gasAmount);
+    System.out.println(station.gasAmount);
   }
 }
