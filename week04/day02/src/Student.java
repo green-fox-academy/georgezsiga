30a6aec0b3cbf2b774338ac799dcf779a6edafa4
georgezsiga <georgezsiga@gmail.com>
2017-04-03 22:16:18 +0200

    Add initial file

---
+++ week04/day02/src/Student.java
@@ -0,0 +1,34 @@
+/**
+ * Created by georgezsiga on 4/3/17.
+ */
+public class Student {
+  String name;
+  int age;
+  String gender;
+  String previousOrganization;
+  int skippedDays;
+
+  public void introduce() {
+    System.out.println("Hi, I`m " + name + ", " + age + " years old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already");
+  }
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer");
+  }
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+}
