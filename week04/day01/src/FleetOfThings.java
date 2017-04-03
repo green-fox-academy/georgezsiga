be1fbba868f457e163a77d29c75421c26a03d49b
georgezsiga <georgezsiga@gmail.com>
2017-04-03 12:08:53 +0200

    Completes the list of tasks

--- week04/day01/src/FleetOfThings.java
+++ week04/day01/src/FleetOfThings.java
@@ -4,12 +4,26 @@
 public class FleetOfThings {
 
   public static void main(String[] args) {
+    Thing getMilk = new Thing("Get Milk");
+    Thing removeObstacles = new Thing("Remove the obstacles");
+    Thing standUp = new Thing("Stand up");
+    Thing eatLunch = new Thing("Eat lunch");
+
+    standUp.complete();
+    eatLunch.complete();
+
     Fleet fleet = new Fleet();
+    fleet.add(getMilk);
+    fleet.add(removeObstacles);
+    fleet.add(standUp);
+    fleet.add(eatLunch);
+
     // Create a fleet of things to have this output:
     // 1. [ ] Get milk
     // 2. [ ] Remove the obstacles
     // 3. [x] Stand up
     // 4. [x] Eat lunch
+
 
     System.out.println(fleet);
   }
