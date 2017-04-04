2f16a86b8d94357d6e3fe800eb043bd48b00b980
georgezsiga <georgezsiga@gmail.com>
2017-04-05 00:16:42 +0200

    Reformat code

--- week04/day01/src/Pirates/Parrot.java
+++ week04/day01/src/Pirates/Parrot.java
@@ -1,10 +1,14 @@
+package Pirates;
+
 /**
  * Created by georgezsiga on 4/4/17.
  */
 public class Parrot extends Pirate {
+
   String name;
 
   public Parrot(String name) {
+    super();
     this.name = name;
   }
 }
