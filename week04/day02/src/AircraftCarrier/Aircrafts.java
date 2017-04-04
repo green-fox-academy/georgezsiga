94214faef5d76307b8275af9a8c099c75e847752
georgezsiga <georgezsiga@gmail.com>
2017-04-04 13:47:48 +0200

    Add initial file with common fields

---
+++ week04/day02/src/AircraftCarrier/Aircrafts.java
@@ -0,0 +1,20 @@
+package AircraftCarrier;
+
+/**
+ * Created by georgezsiga on 4/4/17.
+ */
+public class Aircrafts {
+  int ammo;
+  String name;
+
+  public void flight() {
+
+  }
+
+  public void refill() {
+
+  }
+
+
+
+}
