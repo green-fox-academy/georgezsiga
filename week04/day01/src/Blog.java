9b9b8c9c2b930484a517eff8c24491a97d97cade
georgezsiga <georgezsiga@gmail.com>
2017-04-04 22:25:21 +0200

    Add update method

--- week04/day01/src/Blog.java
+++ week04/day01/src/Blog.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by georgezsiga on 4/3/17.
@@ -9,6 +8,7 @@ public class Blog {
 
   public Blog() {
     this.listOfBlogPosts = new ArrayList<>();
+    addBlogPosts();
   }
 
   public void addBlogPosts() {
@@ -17,7 +17,7 @@ public class Blog {
     listOfBlogPosts.add(blog1);
     BlogPost blog2 = new BlogPost("Tim Urban", "Wait but why", "2010.10.10.");
     blog2.text = "A popular long-form, stick-figure-illustrated blog about almost everything.";
-    listOfBlogPosts.add(blog2)
+    listOfBlogPosts.add(blog2);
     BlogPost blog3 = new BlogPost("William Turton",
         "One Engineer Is Trying to Get IBM to Reckon With Trump", "2017.03.28.");
     blog3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
@@ -28,16 +28,13 @@ public class Blog {
     listOfBlogPosts.remove(index);
   }
 
-  public void update(int index, BlogPost blog) {
-    blogList.set(index, blog);
-
+  public void updateBlogPost(int index, BlogPost blog) {
+    listOfBlogPosts.set(index, blog);
   }
 
   public static void main(String[] args) {
     Blog list1 = new Blog();
-
-    list1.
-
+    list1.addBlogPosts();
+    list1.deleteBlogPost(1);
   }
-
 }
