0b99a6f110708cb800acd7ce29fd16fc0cad518e
georgezsiga <georgezsiga@gmail.com>
2017-04-04 14:42:13 +0200

    Add fields and create an Aircraft Carrier

--- week04/day02/src/AircraftCarrier/AircraftCarrier.java
+++ week04/day02/src/AircraftCarrier/AircraftCarrier.java
@@ -1,8 +1,24 @@
 package AircraftCarrier;
 
+import java.util.ArrayList;
+
 /**
  * Created by georgezsiga on 4/4/17.
  */
 public class AircraftCarrier {
+  int airplaneSlots;
+  int ammoStoreage;
+  int health;
+  ArrayList<Aircrafts> listOfAirplanes;
 
+  public AircraftCarrier(int ammoStoreage) {
+    this.ammoStoreage = ammoStoreage;
+    this.health = 1000;
+    this.airplaneSlots = 10;
+    this.listOfAirplanes = new ArrayList<Aircrafts>();
+  }
+
+  public void addPlanes(String plane) {
+
+  }
 }
