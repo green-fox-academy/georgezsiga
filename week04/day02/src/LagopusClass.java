0b263dd314acfa06c67cb017cd2e608acd3975ff
georgezsiga <georgezsiga@gmail.com>
2017-04-04 10:39:27 +0200

    Adds initial file and fields and methods

---
+++ week04/day02/src/LagopusClass.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+/**
+ * Created by georgezsiga on 4/4/17.
+ */
+public class LagopusClass {
+  String className;
+  List students;
+  List mentors;
+
+  public void addStudent(Student student) {
+    this.students.add(student);
+  }
+
+  public void addStudent(Mentor mentor) {
+    this.mentors.add(mentor);
+  }
+
+  public void info() {
+    System.out.println("Lagopus " + className + " class has " + students.size() + " students and " + mentors.size() + " mentors.");
+  }
+
+  public LagopusClass(String className) {
+    this.className = className;
+    this.students = new List;
+    this.mentors = new List;
+  }
+}
