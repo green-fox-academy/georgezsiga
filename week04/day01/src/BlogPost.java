5a22f90099038b6e9fe326951ba0f91b445cc694
georgezsiga <georgezsiga@gmail.com>
2017-04-04 22:26:24 +0200

    Reformat code

--- week04/day01/src/BlogPost.java
+++ week04/day01/src/BlogPost.java
@@ -23,5 +23,4 @@ public class BlogPost {
         "One Engineer Is Trying to Get IBM to Reckon With Trump", "2017.03.28.");
     blog3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
   }
-
 }
