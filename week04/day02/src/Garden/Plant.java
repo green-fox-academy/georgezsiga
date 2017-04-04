ef8091415914bfa7e4ddda4b230b04de721cdd73
georgezsiga <georgezsiga@gmail.com>
2017-04-04 13:22:03 +0200

    Adds wateringAmount reformat code

---
+++ week04/day02/src/Garden/Plant.java
@@ -0,0 +1,18 @@
+package Garden;
+
+/**
+ * Created by georgezsiga on 4/4/17.
+ */
+public class Plant {
+
+  String color;
+  double waterAmount;
+
+  public void watering(double wateringAmount) {
+
+  }
+
+  public void status() {
+
+  }
+}
