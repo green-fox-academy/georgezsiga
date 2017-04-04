dc0ea7762ab7fa7a582a1c1a208f4bbf482eb283
georgezsiga <georgezsiga@gmail.com>
2017-04-04 16:29:25 +0200

    Adds getters and methods

--- week04/day02/src/AircraftCarrier/Aircrafts.java
+++ week04/day02/src/AircraftCarrier/Aircrafts.java
@@ -6,14 +6,42 @@ package AircraftCarrier;
 public class Aircrafts {
   int ammo;
   int damage;
+  int maxAmmo;
+  int baseDamage;
   String name;
+
+  public Aircrafts() {
+    this(0, 0);
+  }
+
+  public Aircrafts(int maxAmmo, int baseDamage) {
+    this.maxAmmo = maxAmmo;
+    this.baseDamage = baseDamage;
+    ammo = 0;
+    damage = 0;
+    name = "airplane";
+  }
 
   public void refill(int refillAmmo) {
 
   }
 
+  public int getAmmo() {
+    return ammo;
+  }
+
+  public int getMaxAmmo() {
+    return maxAmmo;
+  }
+
+
+
   public int flight() {
     return damage;
+  }
+
+  public void getStatus() {
+
   }
 
 }
