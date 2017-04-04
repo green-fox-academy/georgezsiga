bb704bcca9b62a01a835da73938fe9dfb5d3fc57
georgezsiga <georgezsiga@gmail.com>
2017-04-04 11:27:49 +0200

    Adds status method

--- week04/day02/src/Garden/Tree.java
+++ week04/day02/src/Garden/Tree.java
@@ -5,6 +5,8 @@ package Garden;
  */
 public class Tree extends Garden {
 
+
+
   public Tree(String color, Double waterAmount) {
     this.color = color;
     this.waterAmount = waterAmount;
@@ -12,7 +14,15 @@ public class Tree extends Garden {
 
   public void wateringTree(Double wateringAmount) {
     if (waterAmount < 10) {
-      waterAmount = waterAmount + (wateringAmount*0.4);
+      waterAmount = waterAmount + (wateringAmount * 0.4);
+    }
+  }
+
+  public void status() {
+    if (waterAmount < 10) {
+      System.out.println("The " + color + " tree needs water");
+    } else {
+      System.out.println("The " + color + " tree doesn`t need water");
     }
   }
 }
