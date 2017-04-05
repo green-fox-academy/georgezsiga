b96d7b0bb159c7405135eded3a16059451690c40
georgezsiga <georgezsiga@gmail.com>
2017-04-05 13:19:37 +0200

    Add all tests to fibonacci

--- week04/day03/Test/FibonacciTest.java
+++ week04/day03/Test/FibonacciTest.java
@@ -1,17 +1,30 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
  * Created by georgezsiga on 4/5/17.
  */
 class FibonacciTest {
-Fibonacci fibi = new Fibonacci();
+  @BeforeEach
+  void setUp() {
+    Fibonacci fibi = new Fibonacci();
+  }
 // reference fibonacci number: 0, 1, 1, 2, 3, 5, 8, 13, 21
   @Test
-  void fibonacci() {
+  void fibonacci1() {
+    int n = 1;
+    assertEquals(Fibonacci.fibonacci(n), 0);
+  }
+  @Test
+  void fibonacci2() {
     int n = 2;
     assertEquals(Fibonacci.fibonacci(n), 1);
   }
-
+  @Test
+  void fibonacciLot() {
+    int n = 9;
+    assertEquals(Fibonacci.fibonacci(n), 21);
+  }
 }
