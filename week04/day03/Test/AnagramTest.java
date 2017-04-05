1718a4acdf4fea5f76d0c2fd8d539f893bcee44a
georgezsiga <georgezsiga@gmail.com>
2017-04-05 11:47:57 +0200

    Add initial file

---
+++ week04/day03/Test/AnagramTest.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+class AnagramTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void anagram() {
+  }
+
+}
