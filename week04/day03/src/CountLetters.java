3e2981b7b8f1d9cf7281ca012db67061bc94d6e1
georgezsiga <georgezsiga@gmail.com>
2017-04-05 21:04:26 +0200

    Add HashMap method

---
+++ week04/day03/src/CountLetters.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+public class CountLetters {
+
+  public Map countLetters(String inputString) {
+    inputString = inputString.toLowerCase();
+    HashMap<Character, Integer> dictionaryMap = new HashMap<>();
+    for (int i = 0; i < inputString.length(); ) {
+      int counter = 0;
+      char currentCharacter = inputString.charAt(i);
+      for (int j = 0; j < inputString.length(); j++) {
+        if (inputString.charAt(j) == currentCharacter) {
+          counter++;
+        }
+      }
+      if (!dictionaryMap.containsKey(inputString.charAt(i))) {
+        dictionaryMap.put(inputString.charAt(i), counter++);
+      }
+    }
+    return dictionaryMap;
+  }
+
+}
