7c8f933ea7e54550b02053346c77c312bf4baf79
georgezsiga <georgezsiga@gmail.com>
2017-04-05 15:45:07 +0200

    Add initial file

---
+++ week04/day03/src/Animal.java
@@ -0,0 +1,28 @@
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+public class Animal {
+
+  int hunger;
+  int thirst;
+  String name;
+
+  public Animal(String name) {
+    hunger = 50;
+    thirst = 50;
+    this.name = name;
+  }
+
+  public void eat() {
+    hunger -= 1;
+  }
+
+  public void drink() {
+    thirst -= 1;
+  }
+
+  public void play() {
+    hunger += 1;
+    thirst -= 1;
+  }
+}
