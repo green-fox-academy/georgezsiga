01d0e534d0572033ef6df5353ca1feb86bf00a08
georgezsiga <georgezsiga@gmail.com>
2017-04-05 09:03:42 +0200

    Add initial file

---
+++ week04/day03/src/Apple.java
@@ -0,0 +1,16 @@
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+public class Apple {
+  public String apple;
+
+  public Apple(String apple) {
+    this.apple = apple;
+  }
+
+  public String getApple() {
+    System.out.println(apple);
+    return apple;
+  }
+
+}
