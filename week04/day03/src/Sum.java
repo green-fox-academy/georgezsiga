24ea4ccc030c0348f7aa55a35000dd35a3156604
georgezsiga <georgezsiga@gmail.com>
2017-04-05 10:10:48 +0200

    Add initial file

---
+++ week04/day03/src/Sum.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+public class Sum {
+  ArrayList<Integer> list;
+
+  public Sum() {
+    this.list = new ArrayList<>();
+  }
+
+
+
+}
