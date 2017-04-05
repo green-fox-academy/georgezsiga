c2f8ad2fdf53c0c6a775bb834d3a270ebc3455d0
georgezsiga <georgezsiga@gmail.com>
2017-04-05 11:08:09 +0200

    Add a few more tests

--- week04/day03/Test/SumTest.java
+++ week04/day03/Test/SumTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,17 +8,34 @@ import org.junit.jupiter.api.Test;
  * Created by georgezsiga on 4/5/17.
  */
 class SumTest {
+  ArrayList<Integer> list;
+  Sum sum = new Sum();
 
   @BeforeEach
   void setUp() {
-  }
-
-  @Test
-  void addIntegers() {
+    sum = new Sum();
   }
 
   @Test
   void sumList() {
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    sum.addIntegers(a);
+    sum.addIntegers(b);
+    sum.addIntegers(c);
+    assertEquals(sum.sumList(), 9);
   }
 
+  @Test
+  void sumListEmpty() {
+    assertEquals(sum.sumList(), 0);
+  }
+
+  @Test
+  void sumListOneElement() {
+    int a = 2;
+    sum.addIntegers(a);
+    assertEquals(sum.sumList(), 2);
+  }
 }
