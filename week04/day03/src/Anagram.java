76cfc8e892b7a63ae6ead3351866deebf0fb12e7
georgezsiga <georgezsiga@gmail.com>
2017-04-05 12:03:49 +0200

    Adds good solution for Anagram method

--- week04/day03/src/Anagram.java
+++ week04/day03/src/Anagram.java
@@ -2,19 +2,16 @@
  * Created by georgezsiga on 4/5/17.
  */
 public class Anagram {
-  public static void main(String[] arg) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Please give me two words");
-    String firstWord = scanner.next();
-    String secondWord = scanner.next();
+
+  public boolean anagram(String firstWord, String secondWord) {
     String reverseSecondWord = "";
     for (int i = secondWord.length() - 1; i >= 0; i--) {
       reverseSecondWord = reverseSecondWord + secondWord.charAt(i);
     }
     if (firstWord.equalsIgnoreCase(reverseSecondWord)) {
-      System.out.println("true");
+      return true;
     } else {
-      System.out.println("false");
+      return false;
     }
   }
 }
