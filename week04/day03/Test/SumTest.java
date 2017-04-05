4c3b1079f644436b39980661e650358c1cd790be
georgezsiga <georgezsiga@gmail.com>
2017-04-05 10:17:08 +0200

    Add initial file

---
+++ week04/day03/Test/SumTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by georgezsiga on 4/5/17.
+ */
+class SumTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void addIntegers() {
+  }
+
+  @Test
+  void sumList() {
+  }
+
+}
