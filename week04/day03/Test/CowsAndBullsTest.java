a0a0cb6d0f322baee4994b8a32131544eb037347
georgezsiga <georgezsiga@gmail.com>
2017-04-08 20:39:24 +0200

    Add initial file

---
+++ week04/day03/Test/CowsAndBullsTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by georgezsiga on 4/8/17.
+ */
+class CowsAndBullsTest {
+
+  @Test
+  void compareNumbers() {
+  }
+
+  @Test
+  void guessResult() {
+  }
+
+  @Test
+  void main() {
+  }
+
+}
