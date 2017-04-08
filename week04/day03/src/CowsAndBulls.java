f05eec18b5911b0ab54cff6b0993ac122a950914
georgezsiga <georgezsiga@gmail.com>
2017-04-08 19:34:26 +0200

    Cleans up the code, reformats it.

--- week04/day03/src/CowsAndBulls.java
+++ week04/day03/src/CowsAndBulls.java
@@ -9,18 +9,15 @@ public class CowsAndBulls {
   private int counter = 0;
   private int cow = 0;
   private int bull = 0;
-  private int number;
-  int number1, number2, number3, number4;
-  private boolean isGameRunning;
   private int[] playerGuess;
   private ArrayList<Integer> guessThis;
 
   private int randomNumber() {
-    this.number = (int) (Math.random() * 10);
+    int number = (int) (Math.random() * 10);
     return number;
   }
 
-  public void guessThisNumber() {
+  private void guessThisNumber() {
     this.guessThis = new ArrayList<>();
     guessThis.add(randomNumber());
     guessThis.add(randomNumber());
@@ -32,14 +29,11 @@ public class CowsAndBulls {
     counter++;
   }
 
-  public boolean isGameRunning() {
-    return isGameRunning;
-  }
-
   private int[] guessTheNumbersInput() {
-    System.out.println("Can you guess the four digits of the number I thought of?");
+    System.out.println("Can you guess the four digits of the number I thought of? ex.: 1 2 3 4");
     Scanner scanner = new Scanner(System.in);
-    this.playerGuess = new int[]{scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt()};
+    this.playerGuess = new int[]{scanner.nextInt(), scanner.nextInt(), scanner.nextInt(),
+        scanner.nextInt()};
     this.count();
     return playerGuess;
   }
@@ -48,15 +42,11 @@ public class CowsAndBulls {
     bull = 0;
     cow = 0;
     for (int i = 0; i < playerGuess.length; i++) {
-//      if (playerGuess[i] == guessThis.get(i)) {
-          if (guessThis.contains(playerGuess[i])) {
-        bull +=1;
-//      for (int j = 0; j < guessThis.size(); j++) {
-      if (playerGuess[i] == guessThis.get(i)) {
-//          if (guessThis.contains(playerGuess[i])) {
+      if (guessThis.contains(playerGuess[i])) {
+        bull += 1;
+        if (playerGuess[i] == guessThis.get(i)) {
           cow += 1;
           bull -= 1;
-
         }
       }
     }
@@ -67,7 +57,10 @@ public class CowsAndBulls {
       System.out.println(
           "You have " + cow + " cows, and " + bull + " bulls. You guessed " + counter + " times.");
     } else {
-      System.out.println("Congratulations! Your guess " + playerGuess[0] + playerGuess[1] + playerGuess[2] + playerGuess[3] + " is the number I thought of. Your guessed it in " + counter + " rounds. You have won the game.");
+      System.out.println(
+          "Congratulations! Your guess " + playerGuess[0] + playerGuess[1] + playerGuess[2]
+              + playerGuess[3] + " is the number I thought of. Your guessed it in " + counter
+              + " rounds. You have won the game.");
       System.exit(0);
     }
   }
@@ -76,10 +69,10 @@ public class CowsAndBulls {
     CowsAndBulls newGame = new CowsAndBulls();
     newGame.guessThisNumber();
     System.out.println(newGame.guessThis);
-    while (newGame.cow !=4) {
-    newGame.guessTheNumbersInput();
-    newGame.compareNumbers();
-    newGame.guessResult();
+    while (newGame.cow < 4) {
+      newGame.guessTheNumbersInput();
+      newGame.compareNumbers();
+      newGame.guessResult();
     }
   }
 }
