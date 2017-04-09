4e2c384bc190ef081ea849161f3b6830abff67b9
georgezsiga <georgezsiga@gmail.com>
2017-04-09 11:53:49 +0200

    Removes double printouts at the battle report, and fixes that the looser ship gets destroyed

--- week04/day01/src/Pirates/Armada.java
+++ week04/day01/src/Pirates/Armada.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
 public class Armada {
   ArrayList<Ship> ships;
   static int armadaIDCreated = 1;
-  int id;
+  private int armadaId;
 
   public Armada() {
     ships = new ArrayList<>();
-    id = armadaIDCreated++;
+    armadaId = armadaIDCreated++;
   }
 
   public void fillArmada() {
@@ -24,17 +24,19 @@ public class Armada {
     }
   }
 
+  public int getArmadaID() {
+    return armadaId;
+  }
+
+
   public ArrayList<Ship> getShips() {
     return ships;
   }
 
-  public int getId() {
-    return id;
-  }
 
   public void show() {
     System.out.println(
-        "Armada id: " + id + " has " + (ships.size() +1) + " ships in it.");
+        "Armada id: " + getArmadaID() + " has " + (ships.size() +1) + " ships in it.");
   }
 
   public void armadaWar(Armada otherArmada) {
@@ -42,18 +44,13 @@ public class Armada {
     System.out.println("Will attack this armada:");
     otherArmada.show();
     System.out.println();
-    while (getShips().size() > 0 || otherArmada.getShips().size() > 0) {
+    while (getShips().size() > 0 && otherArmada.getShips().size() > 0) {
       Ship shipThisArmada = getShips().get(0);
       Ship shipOtherArmada = otherArmada.getShips().get(0);
-      getShips().get(0).battle(otherArmada.getShips().get(0));
       if (shipThisArmada.battle(shipOtherArmada)) {
-        show();
-        System.out.println("Has won the battle");
-        getShips().remove(0);
-      } else {
-        show();
-        System.out.println("Has lost the battle");
         otherArmada.getShips().remove(0);
+      } else {
+        getShips().remove(0);
       }
     }
     if (getShips().size() == 0) {
