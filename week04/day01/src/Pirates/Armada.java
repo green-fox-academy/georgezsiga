faa4c3e770b9178428df18af3ffd95468caa1b12
georgezsiga <georgezsiga@gmail.com>
2017-04-09 12:11:02 +0200

    Fixes the random number generator, so an Armada is at least 2 ships

--- week04/day01/src/Pirates/Armada.java
+++ week04/day01/src/Pirates/Armada.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
  * Created by georgezsiga on 4/8/17.
  */
 public class Armada {
+
   ArrayList<Ship> ships;
   static int armadaIDCreated = 1;
   private int armadaId;
@@ -16,7 +17,7 @@ public class Armada {
   }
 
   public void fillArmada() {
-    int shipsInTheArmada = (int) (Math.random() * 10);
+    int shipsInTheArmada = (int) (Math.random() * 9) + 1;
     for (int i = 0; i < shipsInTheArmada; i++) {
       Ship s = new Ship();
       s.fillShip();
@@ -36,7 +37,7 @@ public class Armada {
 
   public void show() {
     System.out.println(
-        "Armada id: " + getArmadaID() + " has " + (ships.size() +1) + " ships in it.");
+        "Armada id: " + getArmadaID() + " has " + (ships.size() + 1) + " ships in it.");
   }
 
   public void armadaWar(Armada otherArmada) {
@@ -45,8 +46,8 @@ public class Armada {
     otherArmada.show();
     System.out.println();
     while (getShips().size() > 0 && otherArmada.getShips().size() > 0) {
-      Ship shipThisArmada = getShips().get(0);
-      Ship shipOtherArmada = otherArmada.getShips().get(0);
+    Ship shipThisArmada = getShips().get(0);
+    Ship shipOtherArmada = otherArmada.getShips().get(0);
       if (shipThisArmada.battle(shipOtherArmada)) {
         otherArmada.getShips().remove(0);
       } else {
@@ -54,11 +55,11 @@ public class Armada {
       }
     }
     if (getShips().size() == 0) {
-      System.out.println("Our Armada has lost the battle. The winner is:");
-      otherArmada.show();
-    } else {
-      System.out.println("Our Armada has won the battle! The winner is:");
-      show();
+        System.out.println("Our Armada has lost the battle. The winner is:");
+        otherArmada.show();
+      } else {
+        System.out.println("Our Armada has won the battle! The winner is:");
+        show();
+      }
     }
   }
-}
