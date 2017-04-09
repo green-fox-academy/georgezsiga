3d163fb495d10a33899b4dc3f3ea2bbc97ed02d1
georgezsiga <georgezsiga@gmail.com>
2017-04-09 12:15:57 +0200

    Removes unused code and reformat it according to Google style guide

--- week04/day01/src/Pirates/BattleApp.java
+++ week04/day01/src/Pirates/BattleApp.java
@@ -6,10 +6,10 @@ package Pirates;
 public class BattleApp {
 
   public static void main(String[] args) {
-    Ship ship1 = new Ship();
-    ship1.fillShip();
-    Ship ship2 = new Ship();
-    ship2.fillShip();
-    ship1.battle(ship2);
+    Armada armada1 = new Armada();
+    armada1.fillArmada();
+    Armada armada2 = new Armada();
+    armada2.fillArmada();
+    armada1.armadaWar(armada2);
   }
 }
