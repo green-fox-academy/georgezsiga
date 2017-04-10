7e96416b48040eef53682b5a55995354d21493d5
georgezsiga <georgezsiga@gmail.com>
2017-04-10 12:59:54 +0200

    Add inital file and the JFRAME method

---
+++ week05/RPGGame2/src/MainLoop.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+/**
+ * Created by georgezsiga on 4/10/17.
+ */
+public class MainLoop {
+  public static void main(String[] args) {
+
+    JFrame frame = new JFrame("RPG Game");
+    Area area = new Area();
+    frame.add(area);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+    frame.pack();
+    frame.addKeyListener(area);
+
+  }
+}
