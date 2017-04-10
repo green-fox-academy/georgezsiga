b90444068d543099e0b22a680e0b8ef373b52066
georgezsiga <georgezsiga@gmail.com>
2017-04-10 14:41:09 +0200

    Add boolean iswall

--- week05/RPGGame2/src/Tile.java
+++ week05/RPGGame2/src/Tile.java
@@ -2,8 +2,14 @@
  * Created by georgezsiga on 4/10/17.
  */
 public class Tile extends GameObject {
+  boolean isWall;
+
+  public boolean isWall() {
+    return isWall;
+  }
 
   public Tile(String filename, int posX, int posY) {
     super(filename, posX, posY);
   }
+
 }
