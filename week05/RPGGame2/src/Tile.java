f649562279aa2dfcde259ff2fc16bc9a13d939f3
georgezsiga <georgezsiga@gmail.com>
2017-04-10 13:03:37 +0200

    Add initial file with constructor

---
+++ week05/RPGGame2/src/Tile.java
@@ -0,0 +1,9 @@
+/**
+ * Created by georgezsiga on 4/10/17.
+ */
+public class Tile extends GameObject {
+
+  public Tile(String filename, int posX, int posY) {
+    super(filename, posX, posY);
+  }
+}
