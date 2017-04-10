11d6cbb36fcf9fce68565b4ef82b7d70946b911b
georgezsiga <georgezsiga@gmail.com>
2017-04-10 12:04:59 +0200

    Rename file

--- week05/RPGGame/src/Hero.java
+++ week05/RPGGame/src/Hero.java
@@ -1,6 +1,14 @@
 /**
  * Created by georgezsiga on 4/10/17.
  */
-public class Hero extends Characters {
+public class Hero extends Character {
+
+  public Hero() {
+  super();
+  this.maxHP = 38;
+  this.currentHP = (20 + rollTheDice());
+  this.defendPoint = (2 * rollTheDice());
+  this.strikePoint = (5 + rollTheDice());
+  }
 
 }
