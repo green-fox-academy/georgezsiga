bd84d89a4d40fe7c2b4c780563380824065d566f
georgezsiga <georgezsiga@gmail.com>
2017-04-10 17:12:14 +0200

    Add initial file with constructor

---
+++ week05/RPGGame2/src/Monster.java
@@ -0,0 +1,9 @@
+/**
+ * Created by georgezsiga on 4/10/17.
+ */
+public class Monster extends Character {
+
+  public Monster(String filename, int posX, int posY) {
+    super(filename, posX, posY);
+  }
+}
