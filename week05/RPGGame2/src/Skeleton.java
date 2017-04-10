5eea0beb09132af59f3a9c3a780f98b83c133c3e
georgezsiga <georgezsiga@gmail.com>
2017-04-10 17:12:04 +0200

    Add initial file with constructor

---
+++ week05/RPGGame2/src/Skeleton.java
@@ -0,0 +1,9 @@
+/**
+ * Created by georgezsiga on 4/10/17.
+ */
+public class Skeleton extends Monster {
+
+  public Skeleton(String filename, int posX, int posY) {
+    super(filename, posX, posY);
+  }
+}
