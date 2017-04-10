1c71cd83c596159130ec57e1af794b1abeb6c397
georgezsiga <georgezsiga@gmail.com>
2017-04-10 13:00:38 +0200

    Add initial file with game object constructor

---
+++ week05/RPGGame2/src/GameObject.java
@@ -0,0 +1,35 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JComponent;
+
+/**
+ * Created by georgezsiga on 4/10/17.
+ */
+public class GameObject extends JComponent {
+  BufferedImage image;
+  int posX, posY;
+
+  public GameObject() {
+
+  }
+
+  public GameObject(String filename, int posX, int posY) {
+    this.posX = posX;
+    this.posY = posY;
+    try {
+      image = ImageIO.read(new File(filename));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  public void draw(Graphics graphics) {
+    if (image != null) {
+      graphics.drawImage(image, posX, posY, null);
+    }
+  }
+}
