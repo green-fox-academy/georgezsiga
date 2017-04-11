c85a1e3d4b34c62b7dd0200a68f5a556b13a13e6
georgezsiga <georgezsiga@gmail.com>
2017-04-11 23:05:47 +0200

    Set initial game level to 1

--- week05/RPGGame2/src/Monster.java
+++ week05/RPGGame2/src/Monster.java
@@ -2,6 +2,7 @@
  * Created by georgezsiga on 4/10/17.
  */
 public class Monster extends Character {
+  int levelOfGame = 1;
 
   public Monster(String filename, int posX, int posY) {
     super(filename, posX, posY);
