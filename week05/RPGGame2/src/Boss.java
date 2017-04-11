6de0cd79bec95b570daf71652b26d3ad90158922
georgezsiga <georgezsiga@gmail.com>
2017-04-11 23:06:33 +0200

    Add toString method

--- week05/RPGGame2/src/Boss.java
+++ week05/RPGGame2/src/Boss.java
@@ -8,7 +8,17 @@ public class Boss extends Monster {
     this.level = 1;
     this.maxHP = (2 * levelOfGame * 6) + 6;
     this.currentHP = (2 * levelOfGame * gameLogic.rollTheDice()) + gameLogic.rollTheDice();
-    this.defendDP = (levelOfGame / 2 * gameLogic.rollTheDice()) + gameLogic.rollTheDice();
+    this.defendDP = ((levelOfGame / 2) * gameLogic.rollTheDice()) + (gameLogic.rollTheDice() / 2);
     this.strikeSP = (levelOfGame * gameLogic.rollTheDice()) + levelOfGame;
+  }
+
+  @Override
+  public String toString() {
+    return "Boss " +
+        "(level " + level +
+        ") HP: " + currentHP +
+        "/" + maxHP +
+        " | DP: " + defendDP +
+        " | SP: " + strikeSP;
   }
 }
