f8f8f26f15d90a24403ca6f90880bf27122fab8b
georgezsiga <georgezsiga@gmail.com>
2017-04-11 23:07:11 +0200

    Add fields to constructor

--- week05/RPGGame2/src/Character.java
+++ week05/RPGGame2/src/Character.java
@@ -2,8 +2,14 @@
  * Created by georgezsiga on 4/10/17.
  */
 public class Character extends GameObject{
+  GameLogic gameLogic = new GameLogic();
 
   public Character(String filename, int posX, int posY) {
     super(filename, posX, posY);
+    this.level = level;
+    this.maxHP = maxHP;
+    this.currentHP = currentHP;
+    this.defendDP = defendDP;
+    this.strikeSP = strikeSP;
   }
 }
