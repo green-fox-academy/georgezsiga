275c9d2d1c704fb4e3d8959d2e61f464cddd28ff
georgezsiga <georgezsiga@gmail.com>
2017-04-11 12:48:24 +0200

    Add boss stats

--- week05/RPGGame2/src/Boss.java
+++ week05/RPGGame2/src/Boss.java
@@ -5,5 +5,10 @@ public class Boss extends Monster {
 
   public Boss(String filename, int posX, int posY) {
     super(filename, posX, posY);
+    this.level = 1;
+    this.maxHP = (2 * levelOfGame * 6) + 6;
+    this.currentHP = (2 * levelOfGame * gameLogic.rollTheDice()) + gameLogic.rollTheDice();
+    this.defendDP = (levelOfGame / 2 * gameLogic.rollTheDice()) + gameLogic.rollTheDice();
+    this.strikeSP = (levelOfGame * gameLogic.rollTheDice()) + levelOfGame;
   }
 }
