5b9e43493d81b6216c7ac8616ff926c7e132bdf1
georgezsiga <georgezsiga@gmail.com>
2017-04-11 12:51:13 +0200

    Add Skeleton stats

--- week05/RPGGame2/src/Skeleton.java
+++ week05/RPGGame2/src/Skeleton.java
@@ -5,5 +5,10 @@ public class Skeleton extends Monster {
 
   public Skeleton(String filename, int posX, int posY) {
     super(filename, posX, posY);
+    this.level = 1;
+    this.maxHP = 2 * levelOfGame * 6;
+    this.currentHP = 2 * levelOfGame * gameLogic.rollTheDice();
+    this.defendDP = (levelOfGame / 2) * gameLogic.rollTheDice();
+    this.strikeSP = levelOfGame * gameLogic.rollTheDice();
   }
 }
