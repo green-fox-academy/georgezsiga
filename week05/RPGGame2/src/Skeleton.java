0021510a77f3d51dc30566d7eca4b857f7d1e212
georgezsiga <georgezsiga@gmail.com>
2017-04-12 14:16:55 +0200

    Add level generator for monsters

--- week05/RPGGame2/src/Skeleton.java
+++ week05/RPGGame2/src/Skeleton.java
@@ -8,8 +8,8 @@ public class Skeleton extends Monster {
 
   public Skeleton(BufferedImage image, int posX, int posY, boolean gotKey, int newMaplevel) {
     super(image, posX, posY);
-    this.level = 1;
     this.mapLevel += newMaplevel;
+    this.level = levelGenerator();
     this.maxHP = 2 * mapLevel * 6;
     this.currentHP = 2 * mapLevel * GameLogic.rollTheDice();
     this.defendDP = (mapLevel / 2) * GameLogic.rollTheDice();
