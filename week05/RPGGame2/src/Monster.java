e63cb554c5e45f467811b9071df80f8d4eeaf323
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:59:00 +0200

    Change the level variable name

--- week05/RPGGame2/src/Monster.java
+++ week05/RPGGame2/src/Monster.java
@@ -4,7 +4,6 @@ import java.awt.image.BufferedImage;
  * Created by georgezsiga on 4/10/17.
  */
 public class Monster extends Character {
-  int levelOfGame = 1;
 
   public Monster(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
