8a9b6de342c039db12fe0052e67621581b6c25cd
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:16:40 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Floor.java
+++ week05/RPGGame2/src/Floor.java
@@ -1,10 +1,12 @@
+import java.awt.image.BufferedImage;
+
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Floor extends Tile {
 
-  public Floor(int posX, int posY) {
-    super("assets/floor.png", posX, posY);
+  public Floor(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
     this.isWall = false;
   }
 }
