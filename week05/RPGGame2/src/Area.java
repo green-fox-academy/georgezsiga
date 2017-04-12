28acd9e331164c9604bb492f6c270dfffbd930fe
georgezsiga <georgezsiga@gmail.com>
2017-04-12 15:32:15 +0200

    Add timer to monster moves

--- week05/RPGGame2/src/Area.java
+++ week05/RPGGame2/src/Area.java
@@ -1,9 +1,12 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Timer;
 
 /**
  * Created by georgezsiga on 4/10/17.
@@ -215,16 +218,20 @@ public class Area extends GameObject implements KeyListener {
     } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
       battle();
     }
-    stepCounter ++;
-    if (stepCounter % 2 == 1) {
+    timer.start();
+    repaint();
+  }
+
+  javax.swing.Timer timer = new javax.swing.Timer(1000, new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
       for (int i = 0; i < monsterMap.size() ; i++) {
         monster = monsterMap.get(i);
         moveMonster();
       }
+      repaint();
     }
-    repaint();
-  }
-
+  });
 
 
   public void isHeroDead(Graphics2D g2d) {
