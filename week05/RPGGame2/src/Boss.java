ba10bfa2da5d25866971645224d1f93e0584f476
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:58:46 +0200

    Change the level variable name

--- week05/RPGGame2/src/Boss.java
+++ week05/RPGGame2/src/Boss.java
@@ -8,10 +8,10 @@ public class Boss extends Monster {
   public Boss(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
     this.level = 1;
-    this.maxHP = (2 * levelOfGame * 6) + 6;
-    this.currentHP = (2 * levelOfGame * GameLogic.rollTheDice()) + GameLogic.rollTheDice();
-    this.defendDP = ((levelOfGame / 2) * GameLogic.rollTheDice()) + (GameLogic.rollTheDice() / 2);
-    this.strikeSP = (levelOfGame * GameLogic.rollTheDice()) + levelOfGame;
+    this.maxHP = (2 * level * 6) + 6;
+    this.currentHP = (2 * level * GameLogic.rollTheDice()) + GameLogic.rollTheDice();
+    this.defendDP = ((level / 2) * GameLogic.rollTheDice()) + (GameLogic.rollTheDice() / 2);
+    this.strikeSP = (level * GameLogic.rollTheDice()) + level;
   }
 
   @Override
