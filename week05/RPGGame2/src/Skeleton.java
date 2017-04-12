6161c3d6cf3de0712aff141e899f94dda9cde3f3
georgezsiga <georgezsiga@gmail.com>
2017-04-12 12:27:48 +0200

    Add newMapLevel field to monsters

--- week05/RPGGame2/src/Skeleton.java
+++ week05/RPGGame2/src/Skeleton.java
@@ -4,22 +4,23 @@ import java.awt.image.BufferedImage;
  * Created by georgezsiga on 4/10/17.
  */
 public class Skeleton extends Monster {
-  boolean gotKey;
 
-  public Skeleton(BufferedImage image, int posX, int posY, boolean gotKey) {
+
+  public Skeleton(BufferedImage image, int posX, int posY, boolean gotKey, int newMaplevel) {
     super(image, posX, posY);
     this.level = 1;
-    this.maxHP = 2 * level * 6;
-    this.currentHP = 2 * level * GameLogic.rollTheDice();
-    this.defendDP = (level / 2) * GameLogic.rollTheDice();
-    this.strikeSP = level * GameLogic.rollTheDice();
+    this.mapLevel += newMaplevel;
+    this.maxHP = 2 * mapLevel * 6;
+    this.currentHP = 2 * mapLevel * GameLogic.rollTheDice();
+    this.defendDP = (mapLevel / 2) * GameLogic.rollTheDice();
+    this.strikeSP = mapLevel * GameLogic.rollTheDice();
     this.gotKey = gotKey;
   }
 
   @Override
   public String toString() {
     return "Skeleton " +
-        "(level " + level +
+        "(level " + mapLevel +
         ") HP: " + currentHP +
         "/" + maxHP +
         " | DP: " + defendDP +
