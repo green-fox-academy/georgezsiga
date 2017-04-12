73e025e643a575b35e3575c7e1d4ae9f6d473e91
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:14:55 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Wall.java
+++ week05/RPGGame2/src/Wall.java
@@ -1,10 +1,12 @@
+import java.awt.image.BufferedImage;
+
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Wall extends Tile {
 
-  public Wall(int posX, int posY) {
-    super("assets/wall.png", posX, posY);
+  public Wall(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
     this.isWall = true;
   }
 }
