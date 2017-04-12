d500ef6937015d92d2dc3f1a2f7ce7025ef39e31
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:14:23 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Hero.java
+++ week05/RPGGame2/src/Hero.java
@@ -1,11 +1,12 @@
+import java.awt.image.BufferedImage;
 
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Hero extends Character {
 
-  public Hero(String filename, int posX, int posY) {
-    super(filename, posX, posY);
+  public Hero(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
     this.level = 1;
     this.maxHP = 38;
     this.currentHP = 20 + (2 * gameLogic.rollTheDice());
