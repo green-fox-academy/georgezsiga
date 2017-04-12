fc35cda5aed1fa258886521ee5b324804e8b4527
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:16:15 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Character.java
+++ week05/RPGGame2/src/Character.java
@@ -1,11 +1,13 @@
+import java.awt.image.BufferedImage;
+
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Character extends GameObject{
   GameLogic gameLogic = new GameLogic();
 
-  public Character(String filename, int posX, int posY) {
-    super(filename, posX, posY);
+  public Character(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
     this.level = level;
     this.maxHP = maxHP;
     this.currentHP = currentHP;
