44f5102cfcc0186fed557e71314863057dc997df
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:14:34 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Skeleton.java
+++ week05/RPGGame2/src/Skeleton.java
@@ -1,10 +1,12 @@
+import java.awt.image.BufferedImage;
+
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Skeleton extends Monster {
 
-  public Skeleton(String filename, int posX, int posY) {
-    super(filename, posX, posY);
+  public Skeleton(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
     this.level = 1;
     this.maxHP = 2 * levelOfGame * 6;
     this.currentHP = 2 * levelOfGame * gameLogic.rollTheDice();
