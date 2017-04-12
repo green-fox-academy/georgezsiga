85ba4043e2c37521b1e772b400b9427c18005222
georgezsiga <georgezsiga@gmail.com>
2017-04-12 12:28:01 +0200

    Changes maxHP set value

--- week05/RPGGame2/src/Boss.java
+++ week05/RPGGame2/src/Boss.java
@@ -5,13 +5,14 @@ import java.awt.image.BufferedImage;
  */
 public class Boss extends Monster {
 
-  public Boss(BufferedImage image, int posX, int posY) {
+  public Boss(BufferedImage image, int posX, int posY, int newMaplevel) {
     super(image, posX, posY);
     this.level = 1;
-    this.maxHP = (2 * level * 6) + 6;
-    this.currentHP = (2 * level * GameLogic.rollTheDice()) + GameLogic.rollTheDice();
-    this.defendDP = ((level / 2) * GameLogic.rollTheDice()) + (GameLogic.rollTheDice() / 2);
-    this.strikeSP = (level * GameLogic.rollTheDice()) + level;
+    this.mapLevel += newMaplevel;
+    this.maxHP = (2 * mapLevel * 6) + 6;
+    this.currentHP = (2 * mapLevel * GameLogic.rollTheDice()) + GameLogic.rollTheDice();
+    this.defendDP = ((mapLevel / 2) * GameLogic.rollTheDice()) + (GameLogic.rollTheDice() / 2);
+    this.strikeSP = (mapLevel * GameLogic.rollTheDice()) + mapLevel;
   }
 
   @Override
