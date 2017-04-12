63d0c08c2b15b38462bb2a258658591004cf7dbf
georgezsiga <georgezsiga@gmail.com>
2017-04-12 14:17:06 +0200

    Add level generator for monsters

--- week05/RPGGame2/src/Boss.java
+++ week05/RPGGame2/src/Boss.java
@@ -7,8 +7,8 @@ public class Boss extends Monster {
 
   public Boss(BufferedImage image, int posX, int posY, int newMaplevel) {
     super(image, posX, posY);
-    this.level = 1;
     this.mapLevel += newMaplevel;
+    this.level = levelGenerator();
     this.maxHP = (2 * mapLevel * 6) + 6;
     this.currentHP = (2 * mapLevel * GameLogic.rollTheDice()) + GameLogic.rollTheDice();
     this.defendDP = ((mapLevel / 2) * GameLogic.rollTheDice()) + (GameLogic.rollTheDice() / 2);
