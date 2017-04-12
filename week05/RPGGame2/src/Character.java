124160491c6ff913568ec1127d8558f8192eede7
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:42:05 +0200

    Delete unused code

--- week05/RPGGame2/src/Character.java
+++ week05/RPGGame2/src/Character.java
@@ -4,14 +4,8 @@ import java.awt.image.BufferedImage;
  * Created by georgezsiga on 4/10/17.
  */
 public class Character extends GameObject{
-  GameLogic gameLogic = new GameLogic();
 
   public Character(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
-    this.level = level;
-    this.maxHP = maxHP;
-    this.currentHP = currentHP;
-    this.defendDP = defendDP;
-    this.strikeSP = strikeSP;
   }
 }
