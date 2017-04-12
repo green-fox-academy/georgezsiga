b37cf7608ecc2653257b1a63e9bd6fd3db37a49d
georgezsiga <georgezsiga@gmail.com>
2017-04-12 14:17:17 +0200

    Add level generator for monsters

--- week05/RPGGame2/src/Character.java
+++ week05/RPGGame2/src/Character.java
@@ -8,4 +8,15 @@ public class Character extends GameObject{
   public Character(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
   }
+
+  public int levelGenerator() {
+    int randNum = GameLogic.randomNumber();
+    if (randNum == 1) {
+      return getMapLevel() + 2;
+    } else if (randNum == 0 || randNum == 2 || randNum == 3 ) {
+      return getMapLevel() + 1;
+    } else {
+      return getMapLevel();
+    }
+  }
 }
