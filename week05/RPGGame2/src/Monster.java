db8fe31b2d91ded230f8b03a3c3288e1f677a30c
georgezsiga <georgezsiga@gmail.com>
2017-04-12 10:15:06 +0200

    Update to Singleton method

--- week05/RPGGame2/src/Monster.java
+++ week05/RPGGame2/src/Monster.java
@@ -1,10 +1,12 @@
+import java.awt.image.BufferedImage;
+
 /**
  * Created by georgezsiga on 4/10/17.
  */
 public class Monster extends Character {
   int levelOfGame = 1;
 
-  public Monster(String filename, int posX, int posY) {
-    super(filename, posX, posY);
+  public Monster(BufferedImage image, int posX, int posY) {
+    super(image, posX, posY);
   }
 }
