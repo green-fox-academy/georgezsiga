f8f8fa5f4d22fd842f20e8007f0564b1272c3244
georgezsiga <georgezsiga@gmail.com>
2017-04-13 09:23:19 +0200

    add iswall field

--- week05/RPGGame2/src/Tile.java
+++ week05/RPGGame2/src/Tile.java
@@ -4,14 +4,10 @@ import java.awt.image.BufferedImage;
  * Created by georgezsiga on 4/10/17.
  */
 public class Tile extends GameObject {
-  boolean isWall;
-
-  public boolean isWall() {
-    return isWall;
-  }
 
   public Tile(BufferedImage image, int posX, int posY) {
     super(image, posX, posY);
+    this.isWall = false;
   }
 
 }
