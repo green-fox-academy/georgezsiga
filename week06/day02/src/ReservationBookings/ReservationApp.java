643faf9a367f73980144b8605d2c336e2fc49d41
georgezsiga <georgezsiga@gmail.com>
2017-04-18 17:22:21 +0200

    Add initial file and main method

---
+++ week06/day02/src/ReservationBookings/ReservationApp.java
@@ -0,0 +1,12 @@
+package ReservationBookings;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class ReservationApp {
+
+  public static void main(String[] args) {
+    Reservation bookings = new Reservation();
+    System.out.println(bookings.toString());
+  }
+}
