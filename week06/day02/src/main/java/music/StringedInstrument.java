6f6bbd6e67af6acf51c7fc3886095de5515c3814
georgezsiga <georgezsiga@gmail.com>
2017-04-18 15:23:14 +0200

    Add initial file with fields

---
+++ week06/day02/src/main/java/music/StringedInstrument.java
@@ -0,0 +1,11 @@
+package main.java.music;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public abstract class StringedInstrument extends Instrument {
+
+  final String formatForPlay ="%s, a %d-stringed instrument that %s\n";
+  int numberOfStrings;
+
+}
