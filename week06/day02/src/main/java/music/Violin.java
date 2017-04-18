579988abd6d50f913db99908e78c48dbb0b272dd
georgezsiga <georgezsiga@gmail.com>
2017-04-18 16:01:30 +0200

    Add initial file with constructor

---
+++ week06/day02/src/main/java/music/Violin.java
@@ -0,0 +1,11 @@
+package main.java.music;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class Violin extends StringedInstrument {
+
+  public Violin(String name, int numberOfStrings) {
+    super(name, numberOfStrings, "Screech");
+  }
+}
