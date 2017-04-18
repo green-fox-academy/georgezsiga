0b41218981ed43ca6181e5a496147c1e5dde6519
georgezsiga <georgezsiga@gmail.com>
2017-04-18 14:47:02 +0200

    Add initial file with abstract methods

---
+++ week06/day02/src/Zoo/Mammal.java
@@ -0,0 +1,17 @@
+package Zoo;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class Mammal extends Animal {
+
+  @Override
+  public void greet() {
+
+  }
+
+  @Override
+  public void wantChild() {
+
+  }
+}
