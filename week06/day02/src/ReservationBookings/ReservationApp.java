438ef4224969890278206fbde4fba114b5da350a
georgezsiga <georgezsiga@gmail.com>
2017-04-18 17:32:39 +0200

    Removes tostring from println

--- week06/day02/src/ReservationBookings/ReservationApp.java
+++ week06/day02/src/ReservationBookings/ReservationApp.java
@@ -7,15 +7,15 @@ public class ReservationApp {
 
   public static void main(String[] args) {
     Reservation bookings = new Reservation();
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
-    System.out.println(bookings.toString());
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
+    System.out.println(bookings);
   }
 }
