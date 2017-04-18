6b51afd5ffd390e9f3045ecdc2d5a84e3a3da78e
georgezsiga <georgezsiga@gmail.com>
2017-04-18 14:52:31 +0200

    Add more fields and abstract methods

--- week06/day02/src/Zoo/Animal.java
+++ week06/day02/src/Zoo/Animal.java
@@ -5,4 +5,19 @@ package Zoo;
  */
 public abstract class Animal {
 
+  int age;
+  String name, gender;
+
+  public abstract void greet();
+
+  public abstract void wantChild();
+
+  public abstract void eat();
+
+  public abstract void drink();
+
+  public abstract void sleep();
+
+  public abstract void move();
+
 }
