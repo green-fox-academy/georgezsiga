50b71219e348d9c540ea64d17069f7d783b7dccf
georgezsiga <georgezsiga@gmail.com>
2017-04-18 14:48:14 +0200

    Add initial file with abstract methods

---
+++ week06/day02/src/Zoo/Bird.java
@@ -0,0 +1,17 @@
+package Zoo;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class Bird extends Animal {
+
+  @Override
+  public void greet() {
+
+  }
+
+  @Override
+  public void wantChild() {
+
+  }
+}
