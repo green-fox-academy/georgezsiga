c66d0396220c4e0ae082a6520933b6b74b2f1c6a
georgezsiga <georgezsiga@gmail.com>
2017-04-18 17:22:44 +0200

    Add initial file

---
+++ week06/day02/src/ReservationBookings/Reservationy.java
@@ -0,0 +1,14 @@
+package ReservationBookings;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public interface Reservationy {
+
+  String getDowBooking();
+
+  String getCodeBooking();
+
+  String toString();
+
+}
