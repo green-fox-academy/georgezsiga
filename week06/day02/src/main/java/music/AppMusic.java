fc6a1bf10c38397fc50f2b193f8188f736549620
georgezsiga <georgezsiga@gmail.com>
2017-04-18 16:02:11 +0200

    Change input fields

--- week06/day02/src/main/java/music/AppMusic.java
+++ week06/day02/src/main/java/music/AppMusic.java
@@ -8,9 +8,9 @@ public class AppMusic {
   public static void main(String[] args) {
 
     System.out.println("Test 1, create Electric Guitar, Bass Guitar and Violin with default strings.");
-    ElectricGuitar guitar = new ElectricGuitar();
-    BassGuitar bassGuitar = new BassGuitar();
-    Violin violin = new Violin();
+    ElectricGuitar guitar = new ElectricGuitar("Electric Guitar", 6);
+    BassGuitar bassGuitar = new BassGuitar("Bass Guitar", 4);
+    Violin violin = new Violin("Violin", 4);
 
     System.out.println("Test 1 Play");
     guitar.play();
@@ -18,8 +18,8 @@ public class AppMusic {
     violin.play();
 
     System.out.println("Test 2, create Electric Guitar, Bass Guitar with 7 and 5 strings .");
-    ElectricGuitar guitar2 = new ElectricGuitar(7);
-    BassGuitar bassGuitar2 = new BassGuitar(5);
+    ElectricGuitar guitar2 = new ElectricGuitar("Electric Guitar",7);
+    BassGuitar bassGuitar2 = new BassGuitar("Bass Guitar",5);
 
     System.out.println("Test 2 Play");
     guitar.play();
