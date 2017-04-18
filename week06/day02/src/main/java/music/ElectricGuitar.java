bd3686daa5b74306b0eeddaaa5ad1c336b374272
georgezsiga <georgezsiga@gmail.com>
2017-04-18 15:59:45 +0200

    Add initial file with constructor

---
+++ week06/day02/src/main/java/music/ElectricGuitar.java
@@ -0,0 +1,11 @@
+package main.java.music;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class ElectricGuitar extends StringedInstrument {
+
+  public ElectricGuitar(String name, int numberOfStrings) {
+    super(name, numberOfStrings, "Twang");
+  }
+}
