8baee2e32b9a17716a29ff2788ab28b697db173c
georgezsiga <georgezsiga@gmail.com>
2017-04-18 14:47:40 +0200

    Add initial file with abstract methods

---
+++ week06/day02/src/Zoo/Reptile.java
@@ -0,0 +1,17 @@
+package Zoo;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class Reptile extends Animal {
+
+  @Override
+  public void greet() {
+
+  }
+
+  @Override
+  public void wantChild() {
+
+  }
+}
