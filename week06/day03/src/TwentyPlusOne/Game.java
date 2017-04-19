c8d7557b8b7827413a5ab17981005a1bc33a8521
georgezsiga <georgezsiga@gmail.com>
2017-04-19 12:44:31 +0200

    Add initial file and random number method

---
+++ week06/day03/src/TwentyPlusOne/Game.java
@@ -0,0 +1,17 @@
+package TwentyPlusOne;
+
+/**
+ * Created by georgezsiga on 4/19/17.
+ */
+public class Game {
+
+  public int dealersHand() {
+    int number = (int) (Math.random()*7 + 15);
+    return number;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
