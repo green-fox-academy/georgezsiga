21c28bd01a73dfc3d7799b7588302a1dbc77b097
georgezsiga <georgezsiga@gmail.com>
2017-04-19 14:11:57 +0200

    Fix the scanner in while loop and reformat code

--- week06/day03/src/TwentyPlusOne/Game.java
+++ week06/day03/src/TwentyPlusOne/Game.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Game {
 
   public int dealersHand() {
-    int number = (int) (Math.random()*7 + 15);
+    int number = (int) (Math.random() * 7 + 15);
     return number;
   }
 
@@ -18,27 +18,35 @@ public class Game {
     Deck deck = new Deck();
     ArrayList<Card> playerCards = new ArrayList<>();
     int dealersScore = game.dealersHand();
-
     System.out.println("The dealer has " + dealersScore + ". Can you beat it?");
     System.out.println("Your cards are:");
     deck.shuffleDeck();
     playerCards.add(deck.drawFirst());
     playerCards.add(deck.drawFirst());
-    System.out.println(playerCards.get(0) + ", " + playerCards.get(1));
+    int score = playerCards.get(0).value + playerCards.get(1).value;
+    System.out.println(playerCards.get(0) + ", " + playerCards.get(1) + " Your score is: " + score);
     System.out.println("Do you want to \"draw\" or \"stop\"?");
     Scanner scanner = new Scanner(System.in);
     String userInput = scanner.nextLine();
-    if (userInput.contains("stop")) {
-      int userScore = playerCards.get(0).value + playerCards.get(1).value;
-      if (userScore > dealersScore && userScore < 22) {
-        System.out.println("Congratulations, you have won");
-        System.exit(0);
-      } else {
-        System.out.println("Sorry, you lost!");
-        System.exit(0);
+    while (!userInput.contains("stop")) {
+      playerCards.add(deck.drawFirst());
+      score = 0;
+      System.out.println("Reminder: the dealer has " + dealersScore);
+      for (Card thiscard : playerCards) {
+        System.out.println(thiscard + " ");
+        score += thiscard.value;
       }
+      System.out.println("Your score is: " + score);
+      System.out.println("Do you want to \"draw\" or \"stop\"?");
+      scanner = new Scanner(System.in);
+      userInput = scanner.nextLine();
     }
-
+    if (score > dealersScore && score < 22) {
+      System.out.println("Congratulations, you have won");
+      System.exit(0);
+    } else {
+      System.out.println("Sorry, you lost!");
+      System.exit(0);
+    }
   }
-
 }
