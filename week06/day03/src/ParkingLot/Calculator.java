bff64f380307c43bc9af981fdab37a25dfdc00d5
georgezsiga <georgezsiga@gmail.com>
2017-04-19 15:50:02 +0200

    Add methods for all basic math methods

--- week06/day03/src/ParkingLot/Calculator.java
+++ week06/day03/src/ParkingLot/Calculator.java
@@ -5,4 +5,27 @@ package ParkingLot;
  */
 public class Calculator {
 
+  public static <T extends Number> int addNumbers(T x, T y) {
+    return x.intValue() + y.intValue();
+  }
+
+  public static <T extends Number> int substractNumbers(T x, T y) {
+    return x.intValue() - y.intValue();
+  }
+
+  public static <T extends Number> int multipleNumbers(T x, T y) {
+    return x.intValue() * y.intValue();
+  }
+
+  public static <T extends Number> double divideNumbers(T x, T y) {
+    try {
+      return x.doubleValue() / y.doubleValue();
+    }
+    catch (ArithmeticException e) {
+      System.out.println("Cant divide by 0");
+    }
+    return 0;
+  }
+
+
 }
