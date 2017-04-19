cf03915e3cf705e55879d222aca43925149a7da6
georgezsiga <georgezsiga@gmail.com>
2017-04-19 17:32:55 +0200

    Add runtime annotation

--- week06/day03/src/Screen/Monitor.java
+++ week06/day03/src/Screen/Monitor.java
@@ -1,9 +1,14 @@
 package Screen;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by georgezsiga on 4/19/17.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Monitor {
 
   String aspectRatio() default "4:3";
+  String classification() default "TFT";
 }
