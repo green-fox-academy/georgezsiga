6ac8272dae306e5ef0f6d209e3ff834ebcceeb3f
georgezsiga <georgezsiga@gmail.com>
2017-04-19 17:33:13 +0200

    Add tostring method

--- week06/day03/src/Screen/Screen43.java
+++ week06/day03/src/Screen/Screen43.java
@@ -6,7 +6,18 @@ package Screen;
 @Monitor
 public class Screen43 extends Screen {
 
-  public Screen43(int width, int height, int resolution) {
-    super(width, height, resolution);
+  public Screen43(int width, int height, int resolution, ScreenSize screenSize) {
+    super(width, height, resolution, screenSize);
+  }
+
+  @Override
+  public String toString() {
+    return "Screen43 - " +
+        "width=" + width +
+        ", height=" + height +
+        ", resolution=" + resolution +
+        ", size=" + size +
+        ", classification=" + classification +
+        ", aspectratio=" + aspectRatio;
   }
 }
