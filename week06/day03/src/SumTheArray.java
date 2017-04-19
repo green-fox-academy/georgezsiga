dcce72d894be98ba479cba5130c1c601a191341e
georgezsiga <georgezsiga@gmail.com>
2017-04-19 15:42:36 +0200

    Add sum method and main method

--- week06/day03/src/SumTheArray.java
+++ week06/day03/src/SumTheArray.java
@@ -3,4 +3,26 @@
  */
 public class SumTheArray {
 
+  public static <T extends Number> int sumArray(T[] inputArray, int index) {
+    int sum = 0;
+    try {
+      for (int i = 0; i < index; i++) {
+        sum = sum + inputArray[i].intValue();
+      }
+      return sum;
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Index is out of bounds");
+      for (int i = 0; i < inputArray.length; i++) {
+        System.out.print(inputArray[i] + ", ");
+      }
+
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    Integer[] array = {2, 3, 5, 1};
+    System.out.println(sumArray(array, 3));
+  }
+
 }
