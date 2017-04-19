cfd1d661d33bd7984d5f626729c49b0dcf1f2f26
georgezsiga <georgezsiga@gmail.com>
2017-04-19 12:03:51 +0200

    Add initial file and enums

---
+++ week06/day03/src/TwentyPlusOne/CardRank.java
@@ -0,0 +1,14 @@
+package TwentyPlusOne;
+
+/**
+ * Created by georgezsiga on 4/19/17.
+ */
+public enum CardRank {
+  ACEONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+
+  private final int value;
+  CardRank(int value) {
+    this.value = value;
+  }
+
+}
