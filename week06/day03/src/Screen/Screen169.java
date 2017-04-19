e14977ac997a286ef5e37c0119425e028a19d1a3
georgezsiga <georgezsiga@gmail.com>
2017-04-19 16:25:59 +0200

    Add annotation

--- week06/day03/src/Screen/Screen169.java
+++ week06/day03/src/Screen/Screen169.java
@@ -3,6 +3,7 @@ package Screen;
 /**
  * Created by georgezsiga on 4/19/17.
  */
+@Monitor(aspectRatio = "16:9")
 public class Screen169 extends Screen {
 
   public Screen169(int width, int height, int resolution) {
