0d0582dc3f00b33f073163954c5d4b224eb1fbed
georgezsiga <georgezsiga@gmail.com>
2017-04-19 16:16:21 +0200

    Add initial file and constructor

---
+++ week06/day03/src/Screen/Screen43.java
@@ -0,0 +1,11 @@
+package Screen;
+
+/**
+ * Created by georgezsiga on 4/19/17.
+ */
+public class Screen43 extends Screen {
+
+  public Screen43(int width, int height, int resolution) {
+    super(width, height, resolution);
+  }
+}
