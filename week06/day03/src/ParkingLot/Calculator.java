aa40b038dd5860b204d97b723601692c339fa205
georgezsiga <georgezsiga@gmail.com>
2017-04-19 15:55:47 +0200

    Reformat code

--- week06/day03/src/ParkingLot/Calculator.java
+++ week06/day03/src/ParkingLot/Calculator.java
@@ -19,18 +19,18 @@ public class Calculator {
 
   public static <T extends Number> double divideNumbers(T x, T y) {
     if (y.doubleValue() == 0) {
-  System.out.println("Cant divide by 0");
-System.exit(0);
-return 0;
+      System.out.println("Cant divide by 0");
+      System.exit(0);
+      return 0;
     } else {
       return x.doubleValue() / y.doubleValue();
     }
   }
 
   public static void main(String[] args) {
-    System.out.println(addNumbers(5,4));
-    System.out.println(substractNumbers(5,4));
-    System.out.println(multipleNumbers(5,4));
-    System.out.println(divideNumbers(5,0));
+    System.out.println(addNumbers(5, 4));
+    System.out.println(substractNumbers(5, 4));
+    System.out.println(multipleNumbers(5, 4));
+    System.out.println(divideNumbers(5, 0));
   }
 }
