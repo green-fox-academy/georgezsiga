a9e3a1b56464c2b589a47ecf2933d738aff1bd9c
georgezsiga <georgezsiga@gmail.com>
2017-04-19 16:15:49 +0200

    Add initial file and constructor

---
+++ week06/day03/src/Screen/Screen219.java
@@ -0,0 +1,11 @@
+package Screen;
+
+/**
+ * Created by georgezsiga on 4/19/17.
+ */
+public class Screen219 extends Screen {
+
+  public Screen219(int width, int height, int resolution) {
+    super(width, height, resolution);
+  }
+}
