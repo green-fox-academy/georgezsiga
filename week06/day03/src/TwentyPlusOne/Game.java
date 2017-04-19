df991555a141675fb7035291c880d63a59860142
georgezsiga <georgezsiga@gmail.com>
2017-04-19 13:21:02 +0200

    Add playerCards list

--- week06/day03/src/TwentyPlusOne/Game.java
+++ week06/day03/src/TwentyPlusOne/Game.java
@@ -1,5 +1,8 @@
 package TwentyPlusOne;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by georgezsiga on 4/19/17.
  */
@@ -11,6 +14,22 @@ public class Game {
   }
 
   public static void main(String[] args) {
+    Game game = new Game();
+    Deck deck = new Deck();
+    ArrayList<Card> playerCards = new ArrayList<>();
+
+    System.out.println("The dealer has " + game.dealersHand() + ". Can you beat it?");
+    System.out.println("Your cards are:");
+    deck.shuffleDeck();
+    playerCards.add(deck.drawFirst());
+    playerCards.add(deck.drawFirst());
+    System.out.println(playerCards.get(0) + ", " + playerCards.get(1));
+    System.out.println("Do you want to \"draw\" or \"stop\"?");
+    Scanner scanner = new Scanner(System.in);
+    String userInput = scanner.nextLine();
+    if (userInput.contains("stop")) {
+
+    }
 
   }
 
