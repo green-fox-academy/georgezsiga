babce3bdffd2bf71d74806b499e0320a90bc8f4c
georgezsiga <georgezsiga@gmail.com>
2017-04-19 17:30:09 +0200

    Add tostring method and conversion of annotations

--- week06/day03/src/Screen/Screen169.java
+++ week06/day03/src/Screen/Screen169.java
@@ -3,10 +3,28 @@ package Screen;
 /**
  * Created by georgezsiga on 4/19/17.
  */
-@Monitor(aspectRatio = "16:9")
+@Monitor(aspectRatio = "16:9", classification = "LCD")
 public class Screen169 extends Screen {
 
-  public Screen169(int width, int height, int resolution) {
-    super(width, height, resolution);
+
+  public Screen169(int width, int height, int resolution, ScreenSize screenSize) {
+    super(width, height, resolution, screenSize);
   }
+  Monitor annotation = Screen169.class.getAnnotation(Monitor.class);
+  String aspectRatio = annotation.aspectRatio();
+  String classification = annotation.classification();
+
+
+  @Override
+  public String toString() {
+    return "Screen169 - " +
+        "width=" + width +
+        ", height=" + height +
+        ", resolution=" + resolution +
+        ", size=" + size +
+        ", classification=" + classification +
+        ", aspectratio=" + aspectRatio;
+
+  }
+
 }
