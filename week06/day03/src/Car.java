d848526928213459dfa9b362dd65bb2615ffd85d
georgezsiga <georgezsiga@gmail.com>
2017-04-19 10:17:40 +0200

    Add fields and constructor

--- week06/day03/src/Car.java
+++ week06/day03/src/Car.java
@@ -2,5 +2,11 @@
  * Created by georgezsiga on 4/19/17.
  */
 public class Car {
+  CarType carType;
+  Color color;
 
+  public Car(CarType carType, Color color) {
+    this.carType = carType;
+    this.color = color;
+  }
 }
