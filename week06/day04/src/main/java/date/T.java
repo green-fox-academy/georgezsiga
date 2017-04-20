74eb7bd4e29bcdebfe16ef126abcf8c8f63de2fe
georgezsiga <georgezsiga@gmail.com>
2017-04-20 13:14:14 +0200

    add constructor

--- week06/day04/src/main/java/date/T.java
+++ week06/day04/src/main/java/date/T.java
@@ -1,8 +1,17 @@
 package date;
 
+import java.time.LocalDate;
+
 /**
  * Created by georgezsiga on 4/20/17.
  */
 public class T {
+
+  LocalDate date;
+
+  public T() {
+    LocalDate date = LocalDate.now();
+    this.date = date;
+  }
 
 }
