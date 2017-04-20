72faa85220432e04e43577058a730db49b799dfa
georgezsiga <georgezsiga@gmail.com>
2017-04-20 09:49:51 +0200

    Add initial file and move the main method here

---
+++ week06/day02/src/CreditCard/CreditCardApp.java
@@ -0,0 +1,30 @@
+package CreditCard;
+
+/**
+ * Created by georgezsiga on 4/18/17.
+ */
+public class CreditCardApp {
+
+  public static void main(String[] args) {
+    CreditCard card = new CreditCard("8752");
+    System.out.println(card);
+    card = new CreditCard("8752");
+    System.out.println(card);
+    card = new CreditCard("1234");
+    System.out.println(card);
+    card = new CreditCard("5346");
+    System.out.println(card);
+    card = new CreditCard("8712");
+    System.out.println(card);
+    card = new CreditCard("1256");
+    System.out.println(card);
+    card = new CreditCard("1874");
+    System.out.println(card);
+    card = new CreditCard("3426");
+    System.out.println(card);
+    card = new CreditCard("7634");
+    System.out.println(card);
+    card = new CreditCard("2452");
+    System.out.println(card);
+  }
+}
