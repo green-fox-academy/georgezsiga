592a787f85a9c0db16ba9be4b43b4ab34e8b5328
georgezsiga <georgezsiga@gmail.com>
2017-04-20 09:48:12 +0200

    Add counter to the name, reformat code

--- week06/day02/src/CreditCard/CreditCard.java
+++ week06/day02/src/CreditCard/CreditCard.java
@@ -3,27 +3,26 @@ package CreditCard;
 /**
  * Created by georgezsiga on 4/18/17.
  */
-public class CreditCard implements CreditCardy{
+public class CreditCard implements CreditCardy {
 
   final String format = "Name=%s CC#=%s CVV=%d (validated)";
-  int sum;
+  int sum, number;
   String ccNumber, codeAccount, name;
-  int counter = 0;
+  static int counter = 0;
 
   public CreditCard(String codeAccount) {
     this.ccNumber = randomCcNumber();
     this.sum = cumeSumCVV(codeAccount);
     this.codeAccount = codeAccount;
-    this.counter += 1;
-    this.name = "ABC" + counter;
-
+    this.number = counter++;
+    this.name = "ABC" + number;
   }
 
   public String randomCcNumber() {
     String numbers = "0123456789";
     String ccNr = "";
-    for (int i = 0; i < 16 ; i++) {
-      int nr = (int) (Math.random()*10);
+    for (int i = 0; i < 16; i++) {
+      int nr = (int) (Math.random() * 10);
       ccNr = ccNr + numbers.charAt(nr);
     }
     return ccNr;
@@ -47,7 +46,7 @@ public class CreditCard implements CreditCardy{
   @Override
   public int cumeSumCVV(String codeAccount) {
     int sum = 0;
-    for (int i = 0; i <codeAccount.length() ; i++) {
+    for (int i = 0; i < codeAccount.length(); i++) {
       sum = sum + codeAccount.charAt(i);
     }
     return sum;
