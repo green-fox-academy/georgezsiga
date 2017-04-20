8bbefc62f1e87b7ef6aad5999f49923197347765
georgezsiga <georgezsiga@gmail.com>
2017-04-20 09:50:12 +0200

    Removes the main method

--- week06/day02/src/CreditCard/CreditCard.java
+++ week06/day02/src/CreditCard/CreditCard.java
@@ -56,27 +56,4 @@ public class CreditCard implements CreditCardy {
   public String toString() {
     return String.format(format, getNameCardholder(), ccNumber, getSumCVV());
   }
-
-  public static void main(String[] args) {
-    CreditCard card = new CreditCard("8752");
-    System.out.println(card);
-    card = new CreditCard("8752");
-    System.out.println(card);
-    card = new CreditCard("1234");
-    System.out.println(card);
-    card = new CreditCard("5346");
-    System.out.println(card);
-    card = new CreditCard("8712");
-    System.out.println(card);
-    card = new CreditCard("1256");
-    System.out.println(card);
-    card = new CreditCard("1874");
-    System.out.println(card);
-    card = new CreditCard("3426");
-    System.out.println(card);
-    card = new CreditCard("7634");
-    System.out.println(card);
-    card = new CreditCard("2452");
-    System.out.println(card);
-  }
 }
