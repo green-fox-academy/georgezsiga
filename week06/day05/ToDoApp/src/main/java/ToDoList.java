d7debbeb26b5a863f826ef059d8e5263708fc1d6
georgezsiga <georgezsiga@gmail.com>
2017-04-21 10:30:45 +0200

    Add fields, constructor and tostring method

--- week06/day05/ToDoApp/src/main/java/ToDoList.java
+++ week06/day05/ToDoApp/src/main/java/ToDoList.java
@@ -1,6 +1,26 @@
+import java.time.LocalDate;
+
 /**
  * Created by georgezsiga on 4/21/17.
  */
 public class ToDoList {
 
+  private static int idCounter = 0;
+  private int id;
+  private String toDo;
+  private LocalDate createdAt, completedAt;
+
+  public ToDoList(String toDo, LocalDate createdAt) {
+    this.id = idCounter++;
+    this.toDo = toDo;
+    this.createdAt = createdAt;
+    this.completedAt = null;
+  }
+
+  @Override
+  public String toString() {
+    if (completedAt == null)
+      return "id: " + id + ", toDo: " + toDo + ", ToDo created: " + createdAt + ", this task is not completed yet";
+    return "id: " + id + ", toDo: " + toDo + ", ToDo created: " + createdAt + ", completed: " + completedAt;
+  }
 }
