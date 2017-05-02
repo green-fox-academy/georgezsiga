38b3bbb1ad64c44912c1157949af28c305c860da
georgezsiga <georgezsiga@gmail.com>
2017-05-02 12:10:41 +0200

    add initial file

---
+++ week07/day02/springVersion3/src/main/java/Controllers/HelloRESTController.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by georgezsiga on 5/2/17.
+ */
+@RestController
+public class HelloRESTController {
+
+  @RequestMapping
+  public void greeting() {
+
+  }
+
+}
