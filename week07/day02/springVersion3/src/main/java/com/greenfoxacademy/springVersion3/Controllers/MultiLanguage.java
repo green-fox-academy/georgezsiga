432d6fa245f39329833f5783edabcd02aceb40b8
georgezsiga <georgezsiga@gmail.com>
2017-05-02 14:35:52 +0200

    Add break line to output

--- week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/MultiLanguage.java
+++ week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/MultiLanguage.java
@@ -11,24 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MultiLanguage {
-  AtomicLong atomicLong = new AtomicLong(0);
+
+  AtomicLong atomicLong = new AtomicLong(1);
 
   @RequestMapping("/languages")
   @ResponseBody
   public String greeting(@RequestParam("name") String name) {
-    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
-        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj",
+        "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+        "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló",
+        "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+        "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry",
+        "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+        "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae",
+        "Sholem Aleychem", "Sawubona"};
     String output = "";
-
-    for (String hello: hellos) {
-      output = output + atomicLong.get() + " " + hello + " " + name + "\n";
+    for (String hello : hellos) {
+      output = output + atomicLong.get() + " " + hello + " " + name + "<br>";
       long expectedvalue = atomicLong.get();
       long newValue = atomicLong.get() + 1;
       atomicLong.compareAndSet(expectedvalue, newValue);
     }
-return output;
+    return output;
   }
-
 }
