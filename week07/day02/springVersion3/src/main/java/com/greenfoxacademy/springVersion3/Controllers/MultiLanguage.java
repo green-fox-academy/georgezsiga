af62f2e1f95e74b462424cfa48ea1a8a32e7d0f3
georgezsiga <georgezsiga@gmail.com>
2017-05-02 16:47:41 +0200

    Add color to output

--- week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/MultiLanguage.java
+++ week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/MultiLanguage.java
@@ -2,6 +2,7 @@ package com.greenfoxacademy.springVersion3.Controllers;
 
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
+import lombok.experimental.var;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MultiLanguage {
 
   AtomicLong atomicLong = new AtomicLong(1);
-  ArrayList<Greeting> entries = new ArrayList<>();
+  ArrayList<Language> entries = new ArrayList<>();
+  ArrayList<String> colors = new ArrayList<>();
   String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj",
       "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
       "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló",
@@ -25,26 +27,32 @@ public class MultiLanguage {
       "Sholem Aleychem", "Sawubona"};
 
   public String getRandomColor() {
+//    String letters = "0123456789ABCDEF";
+//    String color = "#";
+//    for (int i = 0; i < 6; i++ ) {
+//      color += letters.charAt((int) Math.random() * 16);
+//    }
+//    return color;
     String letters = "0123456789ABCDEF";
     String color = "#";
     for (int i = 0; i < 6; i++ ) {
-      color += letters.charAt((int) Math.random() * 16);
+      color += letters.charAt((int) (Math.random() * 16));
     }
     return color;
   }
 
   @RequestMapping("/languages")
-  public String languages(@RequestParam("name") String name, Model list, Model color) {
+  public String languages(@RequestParam("name") String name, Model list) {
 
     for (String hello : hellos) {
-      Greeting greet = new Greeting(atomicLong.get(),hello + " " + name);
-      entries.add(greet);
+      Language lang = new Language(hello + " " + name, getRandomColor());
+      entries.add(lang);
       long expectedvalue = atomicLong.get();
       long newValue = atomicLong.get() + 1;
       atomicLong.compareAndSet(expectedvalue, newValue);
+
     }
     list.addAttribute("entries", entries);
-    color.addAttribute("color", "blue");
     return "languages";
   }
 }
