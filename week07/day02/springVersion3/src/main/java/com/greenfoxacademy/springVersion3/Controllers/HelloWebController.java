d50344a9c46690d26ab937938e802635eae3f76c
georgezsiga <georgezsiga@gmail.com>
2017-05-02 13:58:17 +0200

    add id attribute and atomiclong

--- week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/HelloWebController.java
+++ week07/day02/springVersion3/src/main/java/com/greenfoxacademy/springVersion3/Controllers/HelloWebController.java
@@ -1,8 +1,10 @@
 package com.greenfoxacademy.springVersion3.Controllers;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by georgezsiga on 5/2/17.
@@ -10,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWebController {
 
+  AtomicLong atomicLong = new AtomicLong(0);
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(@RequestParam("name") String name, Model model, Model id) {
+    model.addAttribute("name", name);
+    id.addAttribute("id", atomicLong);
+    long expectedvalue = atomicLong.get();
+    long newValue = atomicLong.get() + 1;
+    atomicLong.compareAndSet(expectedvalue, newValue);
     return "greeting";
   }
 
