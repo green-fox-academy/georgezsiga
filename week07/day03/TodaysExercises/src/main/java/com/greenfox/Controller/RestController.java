313e4552b5fe948e82705f8e4887023f63ea2181
georgezsiga <georgezsiga@gmail.com>
2017-05-03 12:33:42 +0200

    add exercise 4

--- week07/day03/TodaysExercises/src/main/java/com/greenfox/Controller/RestController.java
+++ week07/day03/TodaysExercises/src/main/java/com/greenfox/Controller/RestController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by georgezsiga on 5/3/17.
@@ -20,6 +21,12 @@ public class RestController {
     accounts.add(account);
     model.addAttribute("animalAccount", accounts);
     return "exercise1";
+  }
+
+  @RequestMapping("/exercise4")
+  public String exercise4(Model input) {
+    input.addAttribute("string", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "exercise4";
   }
 
 }
