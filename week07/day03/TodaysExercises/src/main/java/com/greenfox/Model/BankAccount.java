f701c5048b900c62a376fe40cbbdb66f19d4d37a
georgezsiga <georgezsiga@gmail.com>
2017-05-03 11:46:56 +0200

    Add requiredargsconstructor annotation

--- week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
+++ week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
@@ -2,15 +2,16 @@ package com.greenfox.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Created by georgezsiga on 5/3/17.
  */
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class BankAccount {
 
   @Getter String name;
   @Getter String animalType;
   @Getter String balance;
-
 }
