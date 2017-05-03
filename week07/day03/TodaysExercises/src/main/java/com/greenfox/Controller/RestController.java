9eda43b7a22c560b33be0ce8bc352a469f7499c6
georgezsiga <georgezsiga@gmail.com>
2017-05-03 11:15:06 +0200

    Add initial file

---
+++ week07/day03/TodaysExercises/src/main/java/com/greenfox/Controller/RestController.java
@@ -0,0 +1,17 @@
+package com.greenfox.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by georgezsiga on 5/3/17.
+ */
+@Controller
+public class RestController {
+
+  @RequestMapping("/exercise1")
+public String exercise1() {
+
+}
+
+}
