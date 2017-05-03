9ef6f7caff607ad5bf932a367e4c03c7427e7096
georgezsiga <georgezsiga@gmail.com>
2017-05-03 11:27:29 +0200

    add allagrsconstructor annotation

--- week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
+++ week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
@@ -1,14 +1,16 @@
 package com.greenfox.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
  * Created by georgezsiga on 5/3/17.
  */
+@AllArgsConstructor
 public class BankAccount {
 
   @Getter String name;
   @Getter String animalType;
-  @Getter double balance;
+  @Getter String balance;
 
 }
