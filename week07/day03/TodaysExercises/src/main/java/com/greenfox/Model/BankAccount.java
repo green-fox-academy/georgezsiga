0c62be87a46559a0c5cccb7a421e3258b67f42fc
georgezsiga <georgezsiga@gmail.com>
2017-05-03 11:14:38 +0200

    Add lombok getters

--- week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
+++ week07/day03/TodaysExercises/src/main/java/com/greenfox/Model/BankAccount.java
@@ -1,8 +1,14 @@
 package com.greenfox.Model;
 
+import lombok.Getter;
+
 /**
  * Created by georgezsiga on 5/3/17.
  */
 public class BankAccount {
+
+  @Getter String name;
+  @Getter String animalType;
+  @Getter double balance;
 
 }
