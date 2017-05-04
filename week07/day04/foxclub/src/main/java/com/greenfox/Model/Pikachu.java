b08fbeecbcf1994a9423aca9d4f69302f1c8da77
georgezsiga <georgezsiga@gmail.com>
2017-05-04 15:54:50 +0200

    Add annotations and fixes tostring method

--- week07/day04/foxclub/src/main/java/com/greenfox/Model/Pikachu.java
+++ week07/day04/foxclub/src/main/java/com/greenfox/Model/Pikachu.java
@@ -1,11 +1,15 @@
-package com.greenfox;
+package com.greenfox.Model;
 
 import java.util.ArrayList;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Created by georgezsiga on 5/4/17.
  */
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Pikachu {
 
   @Getter String name;
@@ -19,11 +23,12 @@ public class Pikachu {
     this.food = food;
     this.drink = drink;
     this.listOfTricks = new ArrayList<>();
+    listOfTricks.add("You know no tricks yet. Go and learn some!");
   }
 
   @Override
   public String toString() {
-    if (listOfTricks.size() == 0) {
+    if (listOfTricks.get(0).equals("You know no tricks yet. Go and learn some!")) {
       return "This is " + name +
           ". Currently living on " + food +
           " and " + drink +
