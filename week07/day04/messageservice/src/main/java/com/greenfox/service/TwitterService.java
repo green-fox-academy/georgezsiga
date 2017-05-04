de50db8c0eda89fbea01baa391009e5bdacb1fa4
georgezsiga <georgezsiga@gmail.com>
2017-05-04 13:23:45 +0200

    Add fields and tostring method

--- week07/day04/messageservice/src/main/java/com/greenfox/service/TwitterService.java
+++ week07/day04/messageservice/src/main/java/com/greenfox/service/TwitterService.java
@@ -1,12 +1,24 @@
 package com.greenfox.service;
 
+import lombok.Getter;
+
 /**
  * Created by georgezsiga on 5/4/17.
  */
 public class TwitterService implements MessageService {
 
+  @Getter String twitter;
+  @Getter String message;
+
   @Override
   public void sendingMessage() {
+    System.out.println(toString());
+  }
 
+  @Override
+  public String toString() {
+    return "TwitterService: " +
+        "Twitter sent to: " + twitter +
+        " with messsage: " + message;
   }
 }
