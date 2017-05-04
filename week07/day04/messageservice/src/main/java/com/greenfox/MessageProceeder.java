92710c75209b762b240aa9163f6b9d3be6a20ba2
georgezsiga <georgezsiga@gmail.com>
2017-05-04 14:24:55 +0200

    Add constructor

--- week07/day04/messageservice/src/main/java/com/greenfox/MessageProceeder.java
+++ week07/day04/messageservice/src/main/java/com/greenfox/MessageProceeder.java
@@ -1,10 +1,26 @@
 package com.greenfox;
 
+import com.greenfox.configuration.MessageConfiguration;
+import com.greenfox.service.MessageService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by georgezsiga on 5/4/17.
  */
+@Component
 public class MessageProceeder {
 
+  MessageService service;
+
+  public MessageProceeder() {
+    ApplicationContext factory = new AnnotationConfigApplicationContext(MessageConfiguration.class);
+    MessageService service = (MessageService) factory.getBean("EmilService");
+    this.service = service;
+  }
+
   public void processMessage(String s, String s1) {
+    service.sendingMessage(s, s1);
   }
 }
