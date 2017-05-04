2cc696915b19a83edb73c00b502c1cadff4f2af5
georgezsiga <georgezsiga@gmail.com>
2017-05-04 22:54:23 +0200

    Add all actions to action list

--- week07/day04/foxclub/src/main/java/com/greenfox/Controller/MainController.java
+++ week07/day04/foxclub/src/main/java/com/greenfox/Controller/MainController.java
@@ -1,6 +1,8 @@
 package com.greenfox.Controller;
 
 import com.greenfox.Model.Pikachu;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,29 +22,38 @@ public class MainController {
   }
 
   @RequestMapping("/nutritionStore")
-  public String nutritionStore(Model model, Model list) {
-    model.addAttribute("pikachu", pikachu);
-    list.addAttribute("listOfTricks", pikachu.getListOfTricks());
+  public String nutritionStore() {
     return "nutritionStore";
   }
 
   @RequestMapping("/nutritionform")
   public String nutritionForm(String food, String drink) {
+    String action = "Bought " + food + " and " + drink + " from the Nutrition Store on " + LocalDate.now() + " at " + LocalTime
+        .of(LocalTime.now().getHour(), LocalTime.now().getMinute());
+    pikachu.addAction(action);
     pikachu.setFood(food);
     pikachu.setDrink(drink);
     return "redirect:/";
   }
 
   @RequestMapping("/trickCenter")
-  public String trickCenter(Model model, Model list) {
-    model.addAttribute("pikachu", pikachu);
+  public String trickCenter(Model list) {
     list.addAttribute("tricks", pikachu.getTricks());
     return "trickCenter";
   }
 
   @RequestMapping("/trickform")
   public String trickForm(String trick) {
+    String action = "Learned " + trick + " at the Trick Center on " + LocalDate.now() + " at " + LocalTime
+        .of(LocalTime.now().getHour(), LocalTime.now().getMinute());
+    pikachu.addAction(action);
     pikachu.addTrick(trick);
     return "redirect:/";
+  }
+
+  @RequestMapping("/actionHistory")
+  public String actionHistory(Model model) {
+    model.addAttribute("actionHistory", pikachu.getActionHistory());
+    return "actionHistory";
   }
 }
