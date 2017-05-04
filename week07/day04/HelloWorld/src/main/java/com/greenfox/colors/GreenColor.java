4eff9455bebaf4bb8b7377f832a4207437439564
georgezsiga <georgezsiga@gmail.com>
2017-05-04 11:23:54 +0200

    Add initial file

---
+++ week07/day04/HelloWorld/src/main/java/com/greenfox/colors/GreenColor.java
@@ -0,0 +1,12 @@
+package com.greenfox.colors;
+
+/**
+ * Created by georgezsiga on 5/4/17.
+ */
+public class GreenColor implements MyColor{
+
+  @Override
+  public void printColor() {
+    System.out.println("This is green");
+  }
+}
