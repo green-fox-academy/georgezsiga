bcc0209556b6d189f280f6fefd3ab9e81f95c852
georgezsiga <georgezsiga@gmail.com>
2017-05-04 11:13:39 +0200

    Add bean factory

--- week07/day04/HelloWorld/src/main/java/com/greenfox/HelloWorldApplication.java
+++ week07/day04/HelloWorld/src/main/java/com/greenfox/HelloWorldApplication.java
@@ -2,11 +2,18 @@ package com.greenfox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+
+		ApplicationContext factory = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+		HelloWorld hw = factory.getBean(HelloWorld.class);
+		hw.setMessage("Hello World");
+		hw.getMessage();
 	}
 }
