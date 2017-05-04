9d3059c8ec2edb2dd710d3f6da8f14d515ca0368
georgezsiga <georgezsiga@gmail.com>
2017-05-04 13:24:07 +0200

    Add fields and tostring method

--- week07/day04/messageservice/src/main/java/com/greenfox/service/EmilService.java
+++ week07/day04/messageservice/src/main/java/com/greenfox/service/EmilService.java
@@ -1,12 +1,24 @@
 package com.greenfox.service;
 
+import lombok.Getter;
+
 /**
  * Created by georgezsiga on 5/4/17.
  */
 public class EmilService implements MessageService {
 
+  @Getter String email;
+  @Getter String message;
+
   @Override
   public void sendingMessage() {
+    System.out.println(toString());
+  }
 
+  @Override
+  public String toString() {
+    return "EmilService: " +
+        "Email sent to: " + email +
+        " with messsage: " + message;
   }
 }
