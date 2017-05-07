225dd56b12ca1a95414cbc3b0081ba261231d670
georgezsiga <georgezsiga@gmail.com>
2017-05-07 13:09:27 +0200

    Add initial file and type of pokemons

---
+++ week07/day04/foxclub/src/main/java/com/greenfox/Model/TypeOfPokemons.java
@@ -0,0 +1,20 @@
+package com.greenfox.Model;
+
+/**
+ * Created by georgezsiga on 5/7/17.
+ */
+public enum TypeOfPokemons {
+  PIKACHU("pikachu"),
+  CHARMANDER("charmander"),
+  GROWLITHE("growlithe"),
+  MACHOP("machop"),
+  SNORLAX("snorlax"),
+  BAYLEEF("bayleef"),
+  FURRET("furret"),
+  TEDDIURSA("teddiursa");
+
+  private final String name;
+  TypeOfPokemons(String name) {
+    this.name = name;
+  }
+}
