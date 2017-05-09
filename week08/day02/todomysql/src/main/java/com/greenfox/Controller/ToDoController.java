fd7d1e3be9095a239424d38b95fb7008aa134d10
georgezsiga <georgezsiga@gmail.com>
2017-05-09 16:38:03 +0200

    Add request param for listing

--- week08/day02/todomysql/src/main/java/com/greenfox/Controller/ToDoController.java
+++ week08/day02/todomysql/src/main/java/com/greenfox/Controller/ToDoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by georgezsiga on 5/9/17.
@@ -22,7 +23,11 @@ public class ToDoController {
   }
 
   @RequestMapping("/list")
-  public String list(Model model) {
+  public String list(@RequestParam(value = "isActive", required = false) Boolean active, Model model) {
+    if (active != null) {
+      model.addAttribute("todo", toDoRepository.findByisDone(active));
+      return "todolist";
+    }
     model.addAttribute("todo", toDoRepository.findAll());
     return "todolist";
   }
