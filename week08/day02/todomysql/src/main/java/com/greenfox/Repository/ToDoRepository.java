41ebefb0cab2795a65faf89d5e455fcb9a8aa454
georgezsiga <georgezsiga@gmail.com>
2017-05-09 16:41:03 +0200

    Add new search query

--- week08/day02/todomysql/src/main/java/com/greenfox/Repository/ToDoRepository.java
+++ week08/day02/todomysql/src/main/java/com/greenfox/Repository/ToDoRepository.java
@@ -1,11 +1,14 @@
 package com.greenfox.Repository;
 
 import com.greenfox.Model.ToDo;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by georgezsiga on 5/9/17.
  */
 public interface ToDoRepository extends CrudRepository<ToDo, Long> {
+
+  List<ToDo> findByisDone(Boolean string);
 
 }
