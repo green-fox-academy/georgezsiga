871b108e3b335c9b2763aebcc7b113d306b60574
georgezsiga <georgezsiga@gmail.com>
2017-05-09 12:24:32 +0200

    Add autowired and override

--- week08/day02/todosql/src/main/java/com/greenfox/TodosqlApplication.java
+++ week08/day02/todosql/src/main/java/com/greenfox/TodosqlApplication.java
@@ -1,12 +1,23 @@
 package com.greenfox;
 
+import com.greenfox.Repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodosqlApplication {
+public class TodosqlApplication implements CommandLineRunner {
+
+	@Autowired
+	private Repository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosqlApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 }
