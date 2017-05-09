a61fa98c82367bdf86664148f2f4b285115e225d
georgezsiga <georgezsiga@gmail.com>
2017-05-09 12:23:33 +0200

    Add Id and column

--- week08/day02/todosql/src/main/java/com/greenfox/Model/ToDo.java
+++ week08/day02/todosql/src/main/java/com/greenfox/Model/ToDo.java
@@ -1,12 +1,29 @@
 package com.greenfox.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by georgezsiga on 5/9/17.
  */
+@Entity
 public class ToDo {
 
+  @Column
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   int id;
+
+  @Column
   String title;
-  boolean isUrgent = false, isDone = false;
+
+  @Column
+  boolean isUrgent = false;
+
+  @Column
+  boolean isDone = false;
 
 }
