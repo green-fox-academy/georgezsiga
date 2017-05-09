1d423369495e6e4b3f423704ace03b27ec910cbf
georgezsiga <georgezsiga@gmail.com>
2017-05-09 12:48:25 +0200

    add tostirng method

--- week08/day02/todosql/src/main/java/com/greenfox/Model/ToDo.java
+++ week08/day02/todosql/src/main/java/com/greenfox/Model/ToDo.java
@@ -5,11 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by georgezsiga on 5/9/17.
  */
 @Entity
+@NoArgsConstructor
 public class ToDo {
 
   @Column
@@ -26,4 +28,13 @@ public class ToDo {
   @Column
   boolean isDone = false;
 
+
+  public ToDo(String title) {
+    this.title = title;
+  }
+
+  @Override
+  public String toString() {
+    return title;
+  }
 }
