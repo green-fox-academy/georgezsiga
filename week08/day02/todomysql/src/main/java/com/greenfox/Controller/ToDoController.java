db860a25427aa8b4ab324848f670330aad4e56c9
georgezsiga <georgezsiga@gmail.com>
2017-05-09 17:15:11 +0200

    Add delete function

--- week08/day02/todomysql/src/main/java/com/greenfox/Controller/ToDoController.java
+++ week08/day02/todomysql/src/main/java/com/greenfox/Controller/ToDoController.java
@@ -5,6 +5,7 @@ import com.greenfox.Repository.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,14 +45,10 @@ public class ToDoController {
     return "redirect:/";
   }
 
-  @RequestMapping("/delete")
-  public String delete() {
-    return "delete";
-  }
-
-  @RequestMapping("/deleteform")
-  public String deleteform(String title, Model model) {
-
+  @RequestMapping("/{id}/delete")
+  public String delete(@PathVariable long id) {
+    toDoRepository.delete(id);
     return "redirect:/";
   }
+
 }
