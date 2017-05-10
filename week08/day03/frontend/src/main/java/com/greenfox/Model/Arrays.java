ce81d90ab6ff0a039025c7bacd542aa95722841d
georgezsiga <georgezsiga@gmail.com>
2017-05-10 14:47:20 +0200

    Add initial file and fields

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Model/Arrays.java
@@ -0,0 +1,18 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class Arrays {
+
+  String what;
+  Integer[] numbers;
+
+}
