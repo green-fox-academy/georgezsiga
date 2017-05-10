91a4964f02782b0cffbbe143f9ae473c15118b78
georgezsiga <georgezsiga@gmail.com>
2017-05-10 14:39:45 +0200

    add initial file and field

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Model/Until.java
@@ -0,0 +1,17 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class Until {
+
+  Integer until;
+
+}
