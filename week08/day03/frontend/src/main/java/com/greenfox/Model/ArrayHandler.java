636c87800935c4d107a079bb06ea56c0042627a6
georgezsiga <georgezsiga@gmail.com>
2017-05-10 15:10:36 +0200

    Add initial file with fields

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Model/ArrayHandler.java
@@ -0,0 +1,16 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class ArrayHandler {
+  String what;
+  Integer[] numbers;
+  }
