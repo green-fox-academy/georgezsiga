5fdcdd83415476871204f18acfbb65b8e65f6ae0
georgezsiga <georgezsiga@gmail.com>
2017-05-10 14:38:53 +0200

    Add methods

--- week08/day03/frontend/src/main/java/com/greenfox/Controllers/DoUntilRestController.java
+++ week08/day03/frontend/src/main/java/com/greenfox/Controllers/DoUntilRestController.java
@@ -1,5 +1,13 @@
 package com.greenfox.Controllers;
 
+import com.greenfox.Model.DoUntil;
+import com.greenfox.Model.Error;
+import com.greenfox.Model.Until;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,4 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DoUntilRestController {
 
+  Error e = new Error();
+
+  @ExceptionHandler(Exception.class)
+  public String handleAllExceptions() {
+    return "Error happend";
+  }
+
+  @ExceptionHandler(MissingServletRequestParameterException.class)
+  public Error exception(MissingServletRequestParameterException er) {
+    e.setError("Please provide a number!");
+    return e;
+  }
+
+  @PostMapping("/dountil/{what}")
+  public DoUntil doUntil(@RequestBody Until until, @PathVariable String what) {
+    DoUntil d = new DoUntil(until.getUntil(), what);
+    return d;
+  }
 }
