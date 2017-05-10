fae9568fbf61dfff08e129f069d1a6e6ffe3bdad
georgezsiga <georgezsiga@gmail.com>
2017-05-10 10:51:18 +0200

    Add initial file with base endpoint

---
+++ week08/day03/frontend/src/main/java/com/greenfox/MainController.java
@@ -0,0 +1,17 @@
+package com.greenfox;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String homepage() {
+    return "index";
+  }
+
+}
