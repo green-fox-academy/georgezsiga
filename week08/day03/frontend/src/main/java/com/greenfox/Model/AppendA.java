9d3cb6c78652bceaf3c70c8e2b2cc4e0e4ed6abe
georgezsiga <georgezsiga@gmail.com>
2017-05-10 13:27:26 +0200

    add initial file and fields and constructor

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Model/AppendA.java
@@ -0,0 +1,20 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class AppendA {
+
+  String appended;
+
+  public AppendA(String appended) {
+    this.appended = appended + "a";
+  }
+}
