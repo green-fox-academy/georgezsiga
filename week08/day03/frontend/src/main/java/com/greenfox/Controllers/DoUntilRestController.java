2d2583cb5d19bd3204e7cf9e77cf098c20a28b21
georgezsiga <georgezsiga@gmail.com>
2017-05-10 13:41:28 +0200

    add initial file

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Controllers/DoUntilRestController.java
@@ -0,0 +1,11 @@
+package com.greenfox.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@RestController
+public class DoUntilRestController {
+
+}
