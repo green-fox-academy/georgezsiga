1ab21ba79bf91b21dc8e12f85ecb59c262fa15bf
georgezsiga <georgezsiga@gmail.com>
2017-05-10 16:20:07 +0200

    convert swicth to if

--- week08/day03/frontend/src/main/java/com/greenfox/Controllers/ArrayHandlerRestController.java
+++ week08/day03/frontend/src/main/java/com/greenfox/Controllers/ArrayHandlerRestController.java
@@ -1,8 +1,11 @@
 package com.greenfox.Controllers;
 
+import com.greenfox.Model.ArrayHandler;
 import com.greenfox.Model.Arrays;
 import com.greenfox.Model.Error;
 import com.greenfox.Model.Result;
+import com.greenfox.Model.Result2;
+import com.greenfox.Model.Result3;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +32,16 @@ public class ArrayHandlerRestController {
   }
 
   @PostMapping("/arrays")
-  public Result doUntil(@RequestBody Arrays incoming) {
-    Result r = new Result(incoming.getWhat(), incoming.getNumbers());
-    return r;
+  public Result results(@RequestBody ArrayHandler incoming) {
+if (incoming.getWhat().equals("sum")) {
+  Result3 r = new Result3("sum", incoming.getNumbers());
+  return r;
+} else if (incoming.getWhat().equals("multiply")) {
+  Result3 r2 = new Result3("multiply", incoming.getNumbers());
+  return r2;
+} else {
+  Result2 r3 = new Result2(incoming.getNumbers());
+  return r3;
+}
   }
 }
