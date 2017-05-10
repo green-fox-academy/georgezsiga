8c148d503ce5be13954b5c16b48bec7727a06fbf
georgezsiga <georgezsiga@gmail.com>
2017-05-10 12:35:30 +0200

    Add initial file and fields

---
+++ week08/day03/frontend/src/main/java/com/greenfox/Model/Error.java
@@ -0,0 +1,12 @@
+package com.greenfox.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/10/17.
+ */
+@Getter @Setter
+public class Error {
+  String error;
+}
