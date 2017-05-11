ee25a6f9d0f163ddb43d2053d92613a85aba0478
georgezsiga <georgezsiga@gmail.com>
2017-05-12 01:31:54 +0200

    Add to text in enum

--- week07/day04/foxclub/src/main/java/com/greenfox/Model/Links.java
+++ week07/day04/foxclub/src/main/java/com/greenfox/Model/Links.java
@@ -4,12 +4,14 @@ package com.greenfox.Model;
  * Created by georgezsiga on 5/12/17.
  */
 public enum Links {
-  INFORMATION("Information", "/", " Well.. err.. the homepage "),
-  NUTRITIONSTORE("Nutrition Store", "/nutritionStore", " You are what you eat. Give me Fooood!! "),
-  TRICKCENTER("Trick Center", "/trickCenter", " Knowledge is power! Let`s learn something! "),
-  ACTIONHISTORY("Action History", "/actionHistory", " Check out what your Poke did recently "),
-  SLEEP("Go to sleep", "/sleep", " Even pokemons needs to sleep "),
-  LOGIN("Logout", "/login", " If you have better things to do... ");
+  INFORMATION(" - Information - ", "/", " Well.. err.. the homepage "),
+  NUTRITIONSTORE(" - Nutrition Store - ", "/nutritionStore",
+      " You are what you eat. Give me Fooood!! "),
+  TRICKCENTER(" - Trick Center - ", "/trickCenter", " Knowledge is power! Let`s learn something! "),
+  ACTIONHISTORY(" - Action History - ", "/actionHistory",
+      " Check out what your Poke did recently "),
+  SLEEP(" - Go to sleep - ", "/sleep", " Even pokemons needs to sleep "),
+  LOGIN(" - Logout - ", "/login", " If you have better things to do... ");
 
   private final String text;
   private final String link;
