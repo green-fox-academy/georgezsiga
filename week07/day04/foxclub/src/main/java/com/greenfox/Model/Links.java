4acc17f20a09db41a90f179bd0157c57d4cd7ef3
georgezsiga <georgezsiga@gmail.com>
2017-05-12 00:53:51 +0200

    Fill up the fields and add getters

--- week07/day04/foxclub/src/main/java/com/greenfox/Model/Links.java
+++ week07/day04/foxclub/src/main/java/com/greenfox/Model/Links.java
@@ -4,20 +4,32 @@ package com.greenfox.Model;
  * Created by georgezsiga on 5/12/17.
  */
 public enum Links {
-  INFORMATION("Information", "/", ""),
-  NUTRITIONSTORE("Nutrition Store", "/", ""),
-  TRICKCENTER("Trick Center", "/", ""),
-  ACTIONHISTORY("Action History", "/", ""),
-  SLEEP("Go to sleep", "/", ""),
-  LOGIN("Logout", "/", "");
+  INFORMATION("Information", "/", " Well.. err.. the homepage "),
+  NUTRITIONSTORE("Nutrition Store", "/nutritionStore", " You are what you eat. Give me Fooood!! "),
+  TRICKCENTER("Trick Center", "/trickCenter", " Knowledge is power! Let`s learn something! "),
+  ACTIONHISTORY("Action History", "/actionHistory", " Check out what your Poke did recently "),
+  SLEEP("Go to sleep", "/sleep", " Even pokemons needs to sleep "),
+  LOGIN("Logout", "/login", " If you have better things to do... ");
 
-  private final String name;
+  private final String text;
+  private final String link;
+  private final String title;
 
-  TypeOfPokemons(String name) {
-    this.name = name;
+  Links(String text, String link, String title) {
+    this.text = text;
+    this.link = link;
+    this.title = title;
   }
 
-  public String getName() {
-    return name;
+  public String getText() {
+    return text;
+  }
+
+  public String getLink() {
+    return link;
+  }
+
+  public String getTitle() {
+    return title;
   }
 }
