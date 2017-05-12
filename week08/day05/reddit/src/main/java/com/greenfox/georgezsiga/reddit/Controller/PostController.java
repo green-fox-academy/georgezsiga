a5aed838f29b504fd37b6e1c25fbb24b804f2951
georgezsiga <georgezsiga@gmail.com>
2017-05-12 11:30:46 +0200

    delete unused code

--- week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
+++ week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
@@ -1,12 +1,9 @@
 package com.greenfox.georgezsiga.reddit.Controller;
 
-import com.greenfox.georgezsiga.reddit.Model.Post;
 import com.greenfox.georgezsiga.reddit.Model.Posts;
 import com.greenfox.georgezsiga.reddit.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,8 +19,6 @@ public class PostController {
 
   @GetMapping("/posts")
   public Posts posts() {
-    postRepository.save(new Post("Title", "http://valami", "Owner"));
-    postRepository.save(new Post("Title2", "http://valami2", "Owner2"));
     posts = new Posts();
     posts.setPosts(postRepository.findAll());
     return posts;
