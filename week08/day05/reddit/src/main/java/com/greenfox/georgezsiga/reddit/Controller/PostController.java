d16c0dff4acf36e27b02d770903b8dc65a940b08
georgezsiga <georgezsiga@gmail.com>
2017-05-12 12:27:57 +0200

    Add post /posts

--- week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
+++ week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
@@ -1,9 +1,13 @@
 package com.greenfox.georgezsiga.reddit.Controller;
 
+import com.greenfox.georgezsiga.reddit.Model.Post;
 import com.greenfox.georgezsiga.reddit.Model.Posts;
 import com.greenfox.georgezsiga.reddit.Repository.PostRepository;
+import java.sql.Timestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,14 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
   Posts posts;
+  Post post;
 
   @Autowired
   private PostRepository postRepository;
 
   @GetMapping("/posts")
-  public Posts posts() {
+  public Posts getPosts() {
     posts = new Posts();
     posts.setPosts(postRepository.findAll());
     return posts;
+  }
+
+  @PostMapping("/posts")
+  public Post postPosts(@RequestBody Post post) {
+    post.setTimestamp(new Timestamp(System.currentTimeMillis()));
+    postRepository.save(post);
+    return post;
   }
 }
