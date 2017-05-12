b5a2c52e604b7454153822792afe473d36e52e74
georgezsiga <georgezsiga@gmail.com>
2017-05-12 11:25:51 +0200

    add posts endpoint

--- week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
+++ week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Controller/PostController.java
@@ -1,16 +1,31 @@
 package com.greenfox.georgezsiga.reddit.Controller;
 
+import com.greenfox.georgezsiga.reddit.Model.Post;
+import com.greenfox.georgezsiga.reddit.Model.Posts;
 import com.greenfox.georgezsiga.reddit.Repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by georgezsiga on 5/12/17.
  */
-@Controller("/post")
+@RestController("/post")
 public class PostController {
+
+  Posts posts;
 
   @Autowired
   private PostRepository postRepository;
 
+  @GetMapping("/posts")
+  public Posts posts() {
+    postRepository.save(new Post("Title", "http://valami", "Owner"));
+    postRepository.save(new Post("Title2", "http://valami2", "Owner2"));
+    posts = new Posts();
+    posts.setPosts(postRepository.findAll());
+    return posts;
+  }
 }
