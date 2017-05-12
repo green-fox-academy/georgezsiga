d875e6d517f88134f5b376dc131ee52eed8f1588
georgezsiga <georgezsiga@gmail.com>
2017-05-12 10:28:29 +0200

    Add initial file with fields and constructor

---
+++ week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Model/Post.java
@@ -0,0 +1,37 @@
+package com.greenfox.georgezsiga.reddit.Model;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+/**
+ * Created by georgezsiga on 5/12/17.
+ */
+@Getter
+@Setter
+@Entity
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  long id;
+  String title;
+  String href;
+  Timestamp timestamp;
+  int score;
+  String owner;
+  int vote;
+
+  public Post(String title, String href, Timestamp timestamp, int score, String owner, int vote) {
+    this.title = title;
+    this.href = href;
+    this.timestamp = timestamp;
+    this.score = score;
+    this.owner = owner;
+    this.vote = vote;
+  }
+}
