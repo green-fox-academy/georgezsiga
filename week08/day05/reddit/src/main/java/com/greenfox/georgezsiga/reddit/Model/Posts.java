4485bb4e5e5b89811ffcec6c0dafcebdc4a0695d
georgezsiga <georgezsiga@gmail.com>
2017-05-12 10:32:36 +0200

    Add initial file with field

---
+++ week08/day05/reddit/src/main/java/com/greenfox/georgezsiga/reddit/Model/Posts.java
@@ -0,0 +1,18 @@
+package com.greenfox.georgezsiga.reddit.Model;
+
+import java.util.ArrayList;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by georgezsiga on 5/12/17.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class Posts {
+
+  ArrayList<Post> posts;
+
+}
