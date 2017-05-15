9bc6735ce854b6c291e513ec9e8bbc60c7084bd7
georgezsiga <georgezsiga@gmail.com>
2017-05-15 11:36:05 +0200

    Add not ok test

--- week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
+++ week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
@@ -1,14 +1,27 @@
 package com.greenfox.Controller;
 
-import static org.junit.Assert.*;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenfox.GrootApplication;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
  * Created by georgezsiga on 5/15/17.
@@ -19,6 +32,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class GuardianControllerTest {
 
+  private MockMvc mockMvc;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Before
+  public void setup() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
   @Test
   public void handleAllExceptions() throws Exception {
   }
@@ -28,7 +51,21 @@ public class GuardianControllerTest {
   }
 
   @Test
-  public void getPosts() throws Exception {
+  public void grootOk() throws Exception {
+    mockMvc.perform(get("/groot?message=newmessage"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$.received", is("newmessage")))
+        .andExpect(jsonPath("$.translated", is("I am Groot!")));
   }
 
-}
+  @Test
+  public void grootNotOk() throws Exception {
+    mockMvc.perform(get("/groot"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$.error", is("I am Groot!")));
+  }
+
+
+  }
