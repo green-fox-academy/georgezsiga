2a127dd0c3a4f2996423acaf34e5a99b033373d7
georgezsiga <georgezsiga@gmail.com>
2017-05-15 15:34:30 +0200

    add getters and setters

--- week09/day01/groot/src/main/java/com/greenfox/Model/Cargo.java
+++ week09/day01/groot/src/main/java/com/greenfox/Model/Cargo.java
@@ -16,4 +16,44 @@ public class Cargo {
     this.shipstatus = "empty";
     this.ready = false;
   }
+
+  public int getCaliber25() {
+    return caliber25;
+  }
+
+  public void setCaliber25(int caliber25) {
+    this.caliber25 = caliber25;
+  }
+
+  public int getCaliber30() {
+    return caliber30;
+  }
+
+  public void setCaliber30(int caliber30) {
+    this.caliber30 = caliber30;
+  }
+
+  public int getCaliber50() {
+    return caliber50;
+  }
+
+  public void setCaliber50(int caliber50) {
+    this.caliber50 = caliber50;
+  }
+
+  public String getShipstatus() {
+    return shipstatus;
+  }
+
+  public void setShipstatus(String shipstatus) {
+    this.shipstatus = shipstatus;
+  }
+
+  public Boolean getReady() {
+    return ready;
+  }
+
+  public void setReady(Boolean ready) {
+    this.ready = ready;
+  }
 }
