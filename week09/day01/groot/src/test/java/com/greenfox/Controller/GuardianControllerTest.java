a8931ba6b6fa23cea34aedd4efbaa081b380d394
georgezsiga <georgezsiga@gmail.com>
2017-05-15 10:50:50 +0200

    Add annotations

--- week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
+++ week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
@@ -2,11 +2,21 @@ package com.greenfox.Controller;
 
 import static org.junit.Assert.*;
 
+import com.greenfox.GrootApplication;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by georgezsiga on 5/15/17.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GrootApplication.class)
+@WebAppConfiguration
+@EnableWebMvc
 public class GuardianControllerTest {
 
   @Test
