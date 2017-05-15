51fc15c1594158a062ddeb0f5f50c8ddf28ee257
georgezsiga <georgezsiga@gmail.com>
2017-05-15 11:53:41 +0200

    add yondu not ok test

--- week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
+++ week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
@@ -76,6 +76,12 @@ public class GuardianControllerTest {
         .andExpect(jsonPath("$.time", is(10.0)))
         .andExpect(jsonPath("$.speed", is(10.0)));
   }
-
+  @Test
+  public void yonduNotOk() throws Exception {
+    mockMvc.perform(get("/yondu"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(jsonPath("$.error", is("I am Groot!")));
+  }
 
   }
