a827f2ab8e2f04f7d9051c4f99fd733926f67f8c
georgezsiga <georgezsiga@gmail.com>
2017-05-15 16:36:55 +0200

    add initial file and fields and constructor and getters and setters

---
+++ week09/day01/groot/src/main/java/com/greenfox/Model/Food.java
@@ -0,0 +1,41 @@
+package com.greenfox.Model;
+
+/**
+ * Created by georgezsiga on 5/15/17.
+ */
+public class Food {
+
+  String name;
+  int amount;
+  int calorie;
+
+  public Food(String name, int amount, int calorie) {
+    this.name = name;
+    this.amount = amount;
+    this.calorie = calorie;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAmount() {
+    return amount;
+  }
+
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+
+  public int getCalorie() {
+    return calorie;
+  }
+
+  public void setCalorie(int calorie) {
+    this.calorie = calorie;
+  }
+}
