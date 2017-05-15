3e3e5674de8717030ec250cf7025dfb2e2464035
georgezsiga <georgezsiga@gmail.com>
2017-05-15 12:05:56 +0200

    Add initial file with fields constructor and getters setters

---
+++ week09/day01/groot/src/main/java/com/greenfox/Model/Cargo.java
@@ -0,0 +1,59 @@
+package com.greenfox.Model;
+
+/**
+ * Created by georgezsiga on 5/15/17.
+ */
+public class Cargo {
+
+  int caliber25, caliber30, caliber50;
+  String shipstatus;
+  Boolean ready;
+
+  public Cargo() {
+    this.caliber25 = 0;
+    this.caliber30 = 0;
+    this.caliber50 = 0;
+    this.shipstatus = "empty";
+    this.ready = false;
+  }
+
+  public int getCaliber25() {
+    return caliber25;
+  }
+
+  public void setCaliber25(int caliber25) {
+    this.caliber25 = caliber25;
+  }
+
+  public int getCaliber30() {
+    return caliber30;
+  }
+
+  public void setCaliber30(int caliber30) {
+    this.caliber30 = caliber30;
+  }
+
+  public int getCaliber50() {
+    return caliber50;
+  }
+
+  public void setCaliber50(int caliber50) {
+    this.caliber50 = caliber50;
+  }
+
+  public String getShipstatus() {
+    return shipstatus;
+  }
+
+  public void setShipstatus(String shipstatus) {
+    this.shipstatus = shipstatus;
+  }
+
+  public Boolean getReady() {
+    return ready;
+  }
+
+  public void setReady(Boolean ready) {
+    this.ready = ready;
+  }
+}
