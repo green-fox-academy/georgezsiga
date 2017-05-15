0da06683f7b7265abd10375363323b3196ae5e75
georgezsiga <georgezsiga@gmail.com>
2017-05-15 17:39:10 +0200

    Add addfood and removefood and changeAmount methods

--- week09/day01/groot/src/main/java/com/greenfox/Model/CalorieTable.java
+++ week09/day01/groot/src/main/java/com/greenfox/Model/CalorieTable.java
@@ -13,11 +13,31 @@ public class CalorieTable {
     this.calorieTable = new ArrayList<>();
   }
 
+  public void addFood(Food food) {
+    calorieTable.add(food);
+  }
+
+  public void removeFood(String name) {
+    for (Food f : calorieTable) {
+      if (f.getName().equals(name)) {
+        calorieTable.remove(f);
+      }
+    }
+  }
+
   public ArrayList<Food> getCalorieTable() {
     return calorieTable;
   }
 
   public void setCalorieTable(ArrayList<Food> calorieTable) {
     this.calorieTable = calorieTable;
+  }
+
+  public void changeAmount(String name, int amount) {
+    for (Food f : calorieTable) {
+      if (f.getName().equals(name)) {
+        f.setAmount(amount);
+      }
+    }
   }
 }
