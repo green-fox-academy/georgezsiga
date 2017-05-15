2c1abf50124064d2ed92a96de4508f38312ac4cd
georgezsiga <georgezsiga@gmail.com>
2017-05-15 10:46:33 +0200

    Add test for controller

---
+++ week09/day01/groot/src/test/java/com/greenfox/Controller/GuardianControllerTest.java
@@ -0,0 +1,24 @@
+package com.greenfox.Controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by georgezsiga on 5/15/17.
+ */
+public class GuardianControllerTest {
+
+  @Test
+  public void handleAllExceptions() throws Exception {
+  }
+
+  @Test
+  public void exception() throws Exception {
+  }
+
+  @Test
+  public void getPosts() throws Exception {
+  }
+
+}
