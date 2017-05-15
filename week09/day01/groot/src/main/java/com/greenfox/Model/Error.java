8ad6c2f126b3db974b51f86d1c559be620b22dbb
georgezsiga <georgezsiga@gmail.com>
2017-05-15 10:27:19 +0200

    Add initial file and getter setter and constructor

---
+++ week09/day01/groot/src/main/java/com/greenfox/Model/Error.java
@@ -0,0 +1,21 @@
+package com.greenfox.Model;
+
+/**
+ * Created by georgezsiga on 5/15/17.
+ */
+public class Error {
+
+  String error;
+
+  public Error(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+}
