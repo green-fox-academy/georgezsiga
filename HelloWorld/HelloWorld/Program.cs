12829a6c2365dcadeec5d807a1ee77d767722356
georgezsiga <georgezsiga@gmail.com>
2017-07-03 11:55:20 +0200

    add c sharp exercices

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine ("Hello Mono World");
+            
+            int[] numbers = new int[] { 97, 92, 81, 60, 69, 12, 95, 15 };
+
+            IEnumerable<int> firstList = numbers.Where(y => y % 2 == 0);
+            
+            foreach (int e in firstList)
+            {
+                Console.WriteLine(e);
+            }
+            
+            double averageOfOddNumbers = numbers.Where(y => y % 2 == 1).Average();
+            
+            Console.WriteLine("average of odd numbers: " + averageOfOddNumbers);
+            
+            IEnumerable<int> thirdList = numbers.Where(y => y > 0);
+            
+            foreach (int e in thirdList)
+            {
+                Console.WriteLine(e * e);
+            }
+        }
+    }
+}
