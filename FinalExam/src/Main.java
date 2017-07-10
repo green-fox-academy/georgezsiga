d82172ca793a89db18b09e9c16042b84eef91980
georgezsiga <georgezsiga@gmail.com>
2017-07-10 10:15:51 +0200

    add method

---
+++ FinalExam/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by georgezsiga on 7/10/17.
+ */
+public class Main {
+
+  String space = " ";
+  List<String> listOfStrings = Arrays.asList("ab", "cde", "fg");
+
+  public static String ConnectStrings(List listOfStrings, String space) {
+    String connectedString = "";
+    for (int i = 0; i < listOfStrings.size(); i++) {
+      if (i < listOfStrings.size() - 1) {
+        connectedString = connectedString + listOfStrings.get(i) + space;
+      } else {
+        connectedString = connectedString + listOfStrings.get(i);
+      }
+    }
+    return connectedString;
+  }
+
+}
